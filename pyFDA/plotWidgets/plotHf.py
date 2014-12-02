1164a0b786fd34a49a6d335fc8fe48cacf211ad7
chipmuenk <mail@chipmuenk.de>
2014-12-02 09:42:41 +0100

    Major rework, base plotHf on MplWidget Signed-off-by: chipmuenk <mail@chipmuenk.de>

--- pyFDA/plotWidgets/plotHf.py
+++ pyFDA/plotWidgets/plotHf.py
@@ -6,6 +6,9 @@ Edited by Christian Münker, 2013
 import sys
 # import EITHER PyQt4 OR PySide, depending on your system
 from PyQt4 import QtGui, QtCore
+
+from PyQt4.QtGui import QSizePolicy
+from PyQt4.QtCore import QSize
 #from PySide.QtCore import *
 #from PySide.QtGui import *
 
@@ -16,56 +19,48 @@ from PyQt4 import QtGui, QtCore
 
 import numpy as np
 import scipy.signal as sig
-from plotUtils import MplCanvas, MplWidget, MplWidget2
+from plotUtils import MplCanvas, MplWidget
 
 
 N_FFT = 2048 # FFT length for freqz
  
-DEBUG = True      
-        
+DEBUG = True   
+
+"""
+QMainWindow is a class that understands GUI elements like a toolbar, statusbar,
+central widget, docking areas. QWidget is just a raw widget.
+When you want to have a main window for you project, use QMainWindow.
+
+If you want to create a dialog box (modal dialog), use QWidget, or, 
+more preferably, QDialog   
+"""       
+    
 class PlotHf(QtGui.QMainWindow):
 
-    def __init__(self):        
-        super(PlotHf, self).__init__() # initialize QWidget base class
-#        QtGui.QMainWindow.__init__(self) # create main window from base class
+    def __init__(self, parent = None): # default parent = None -> top Window 
+        super(PlotHf, self).__init__(parent) # initialize QWidget base class
+#        QtGui.QMainWindow.__init__(self) # alternative syntax
         
         self.coeffs = ([1,1,1],[3,0,0]) # dummy definition for notch filter
-        self.A_SB = 60   # min. Sperrdämpfung im Stoppband in dB (= min. y-Wert des Plots)
-      
-        self.mplwidget = MplCanvas()
-#       self.mplwidget = MplWidget2()
-        
+        self.A_SB = 60   # min. Sperrdämpfung im Stoppband in dB (= min. y-Wert des Plots)    
+ 
+        self.mplwidget = MplWidget()
+#        self.mplwidget.setParent(self)
         self.mplwidget.setFocus()
+        # make this the central widget, taking all available space:
         self.setCentralWidget(self.mplwidget)
         
-      
+        self.draw(self.coeffs)
         
-        self.update(self.coeffs)
-#        self.plot(self.mplwidget.axes)
-
-#        plotAll.createMPLCanvas(parent)   
-
-
-#        self.setLayout(plotUtils.CreateMPLCanvas().vbox) 
-##       self.pltCanv.setParent(self)
-##        self.pltCanv.setParent(plotUtils.CreateMPLCanvas())
-##        self.pltCanv = FigureCanvas(self.pltFig)
-
-
-
-        
-#        self.setLayout(self.myCanv.vbox)
-
 #        #=============================================
 #        # Signals & Slots
 #        #=============================================          
-#        self.butDraw.clicked.connect(lambda: self.update(self.coeffs))
-#        self.cboxGrid.clicked.connect(self.redraw)
-#        self.sldLw.valueChanged.connect(self.redraw)  
+#        self.mplCanv.butDraw.clicked.connect(lambda: self.draw(self.coeffs))
+        self.mplwidget.sldLw.valueChanged.connect(lambda:self.draw(self.coeffs))  
             
-    def update(self, coeffs):
+    def draw(self, coeffs):
         """ 
-        Re-calculate |H(f)| and update the figure
+        Re-calculate |H(f)| and draw the figure
         """
         self.coeffs = coeffs
         self.bb = self.coeffs[0]
@@ -74,33 +69,24 @@ class PlotHf(QtGui.QMainWindow):
         print 
         if DEBUG:
             print("-------------------------")
-            print("plotHf.update() ") 
+            print("plotHf.draw() ") 
             print("-------------------------")
             print("b,a = ", self.bb, self.aa)
         F = W / (2 * np.pi)
 
-        # clear the axes and redraw the plot
+        # clear the axes and (re)draw the plot
         #
         mpl = self.mplwidget.ax
         mpl.clear()
-        mpl.plot(F,20*np.log10(abs(H)) )#, lw = self.sldLw.value())
+        mpl.plot(F,20*np.log10(abs(H)), lw = self.mplwidget.sldLw.value()/5.)
         mpl.axis([0, 0.5, -self.A_SB-10, 2])
         mpl.set_title(r'Betragsfrequenzgang')
         mpl.set_xlabel(r'$F\; \rightarrow $')    
         mpl.set_ylabel(r'$|H(\mathrm{e}^{\mathrm{j} \Omega})|\; \rightarrow $')    
  
-        self.mplwidget.figure.tight_layout()  
-        self.mplwidget.redrawPlt()
+        self.mplwidget.redraw()
+#        self.mplCanv.pltCanv.draw()
         
-    def redraw(self):
-        """
-        Redraw the figure with new properties (grid, linewidth)
-        """
-#        self.mplwidget.axes.grid(self.cboxGrid.isChecked())
-#        plt.artist.setp(self.pltPlt, linewidth = self.sldLw.value()/5.)
-        self.mplwidget.figure.tight_layout()
-        self.mplwidget.redrawPlt()
-
 #------------------------------------------------------------------------------  
     
 def main():
