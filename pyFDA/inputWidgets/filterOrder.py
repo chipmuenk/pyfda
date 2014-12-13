841b4908d8ecee4267321f37f4f6f8b99d6f576e
chipmuenk <mail@chipmuenk.de>
2014-12-13 10:58:55 +0100

    Layout rework Signed-off-by: chipmuenk <mail@chipmuenk.de>

--- pyFDA/inputWidgets/filterOrder.py
+++ pyFDA/inputWidgets/filterOrder.py
@@ -8,7 +8,7 @@ Datum:12.11.2013
 import sys
 from PyQt4 import QtGui
 
-class FilterOrder(QtGui.QWidget):
+class FilterOrder(QtGui.QFrame):
     """
     Widget for selecting either 
     - manual filter order, specified by an integer 
@@ -20,52 +20,66 @@ class FilterOrder(QtGui.QWidget):
         self.initUI()
         
         
-    def initUI(self): 
+    def initUI(self):
+        bfont = QtGui.QFont()
+        ifont = QtGui.QFont()
+  #      font.setPointSize(11)
+        bfont.setBold(True)
+        bfont.setWeight(75)
+        ifont.setItalic(True)
      
-        self.chkManual = QtGui.QRadioButton("Specify Order",self)
-        self.chkMin = QtGui.QRadioButton("Minimum Order",self)
-        self.group = QtGui.QButtonGroup(self)
-        self.group.addButton(self.chkManual)
-        self.group.addButton(self.chkMin)
-        self.group.setExclusive(True)
+#        self.chkManual = QtGui.QRadioButton("Specify Order",self)
+        self.titleLabel = QtGui.QLabel("Filter Order")
+        self.titleLabel.setFont(bfont)
+        self.chkMin = QtGui.QRadioButton("Minimum",self)
+        self.spacer = QtGui.QSpacerItem(40,0,QtGui.QSizePolicy.Expanding,QtGui.QSizePolicy.Minimum)
+        self.txtLabel = QtGui.QLabel("N = ")
+        self.txtLabel.setFont(ifont)
         
         self.txtManual=QtGui.QLineEdit("10",self)
-        self.txtManual.setEnabled(False)
-        self.chkManual.setChecked(True) 
-   
+ #       self.txtManual.setSizePolicy(QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Minimum)
+
         """
         LAYOUT 
         """
         vbox = QtGui.QHBoxLayout()
-        vbox.addWidget(self.chkManual)
+        vbox.addWidget(self.chkMin)
+        vbox.addItem(self.spacer)
+        vbox.addWidget(self.txtLabel)
+
         vbox.addWidget(self.txtManual)
         layout = QtGui.QVBoxLayout()
+        layout.addWidget(self.titleLabel)
         layout.addItem(vbox)
-        layout.addWidget(self.chkMin)
         self.setLayout(layout)
+        
+#        self.txtManual.setEnabled(False)
+        self.chkMin.setChecked(True) 
         """
         SIGNALS & SLOTs
         """
-        self.chkManual.clicked.connect(self.enableTxt)
+#        self.chkManual.clicked.connect(self.enableTxt)
         self.chkMin.clicked.connect(self.enableTxt)
         
         self.enableTxt() # initialize with default settings
         
     def enableTxt(self):
         """
-        Text input is only possible wenn "Manual" is checked
+        Text input is only possible wenn "Min" is unchecked
         """
 
-        if self.chkManual.isChecked() == True:
-            self.txtManual.setEnabled(True)
+        if self.chkMin.isChecked() == True:
+            self.txtManual.setEnabled(False)
+            self.txtLabel.setEnabled(False)
         else:
-            self.txtManual.setEnabled(False)     
+            self.txtManual.setEnabled(True)
+            self.txtLabel.setEnabled(True)
    
     def get(self):
          """
          Return either the entered filter order or 'min'
          """
-         if self.chkManual.isChecked() == False:
+         if self.chkMin.isChecked() == True:
              ordn = "min"
          else:
              ordn = int(self.txtManual.text())
@@ -73,12 +87,12 @@ class FilterOrder(QtGui.QWidget):
          
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
-    form = widgetFilterOrder()
+    form = FilterOrder()
     form.show()
-    form.chkManual.setChecked(True)
+#    form.chkMin.setChecked(True)
     t=form.get()
     print t
-    form.chkMin.setChecked(True)
+#    form.chkMin.setChecked(False)
     t=form.get()
     print t
     app.exec_()
