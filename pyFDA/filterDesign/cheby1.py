b961b4fc18024c6bff852febaf50101bf8bf1e79
chipmuenk <mail@chipmuenk.de>
2015-01-21 14:37:08 +0100

    Added function get_params() to access the passed dictionary and transform parameters if needed -> correction of frequency specs

--- pyFDA/filterDesign/cheby1.py
+++ pyFDA/filterDesign/cheby1.py
@@ -15,6 +15,8 @@ from __future__ import print_function, division, unicode_literals
 import scipy.signal as sig
 #import numpy as np
 
+import filterbroker as fb
+
 output = 'zpk' # set output format of filter design routines to 'zpk' or 'ba'
 
 class cheby1(object):
@@ -29,8 +31,8 @@ class cheby1(object):
         # enabled widgets and common messages for man. / min. filt. order        
         enb_man = ['fo','fspec','aspec'] # enabled widget for man. filt. order
         enb_min = ['fo','fspec','aspec'] # enabled widget for min. filt. order
-        self.com = {"man":{"enb":enb_man, "msg":msg_man},
-                    "min":{"enb":enb_min, "msg":msg_min}}
+        self.com = {"man":{"enb":enb_man, "msg":msg_man}, # "par":['N', 'f_S']},
+                    "min":{"enb":enb_min, "msg":msg_min}} #, "par":['f_S']}}
 
         self.ft = 'IIR'
         self.rt = {
@@ -40,7 +42,7 @@ class cheby1(object):
                  "min":{"par":['A_pb','A_sb','F_sb','F_pb']}},
           "BP": {"man":{"par":['N', 'A_pb', 'F_pb', 'F_pb2']},
                  "min":{"par":['A_pb','A_sb','F_sb','F_pb','F_pb2','F_sb2']}},
-          "BS": {"man":{"par":['A_pb','F_pb','F_pb2']},
+          "BS": {"man":{"par":['N','A_pb','F_pb','F_pb2']},
                  "min":{"par":['A_pb','A_sb','F_pb','F_sb','F_sb2','F_pb2']}}
                  }
 
@@ -49,7 +51,23 @@ class cheby1(object):
         und über die kritische(n) Frequenz(en) bei denen die Verstärkung unter \
         den spezifizierten Wert fällt."
 
-    def save(self, arg):
+    def get_params(self,specs):
+        """
+        Translate parameters from the passed dictionary to instance
+        parameters, scaling / transforming them if needed.
+        """
+        f_S = specs['f_S'] 
+        self.N     = specs['N']
+        self.F_pb  = specs['F_pb']/f_S * 2
+        self.F_sb  = specs['F_sb']/f_S * 2 
+        self.F_pb2 = specs['F_pb2']/f_S * 2
+        self.F_sb2 = specs['F_sb2']/f_S * 2 
+        self.A_pb  = specs['A_pb']
+        self.A_sb  = specs['A_sb']
+        self.A_pb2 = specs['A_pb2']
+        self.A_sb2 = specs['A_sb2']
+
+    def save(self, specs, arg):
         """ 
         Convert between poles / zeros / gain, filter coefficients (polynomes) 
         and second-order sections and store all available formats in the global
@@ -58,50 +76,56 @@ class cheby1(object):
         if output == 'zpk': # arg = [z,p,k]
             self.coeffs = sig.zpk2tf(arg[0], arg[1], arg[2])
             self.zpk = arg
-
         else: # arg = [b,a]
             self.zpk = sig.tf2zpk(arg[0], arg[1])
             self.coeffs = arg
 
+        specs['coeffs'] = self.coeffs
+        specs['zpk'] = self.zpk
+
+
     def LPman(self, specs):
-        self.save(sig.cheby1(specs['N'], specs['A_pb'], specs['F_pb'],
-                              btype='low', analog = False, output = output))
+        self.get_params(specs)
+        self.save(specs, sig.cheby1(self.N, self.A_pb, self.F_pb,
+                            btype='low', analog = False, output = output))
 
     def HPman(self, specs):
-        self.save(sig.cheby1(specs['N'], specs['A_pb'], specs['F_pb'], 
-                             btype='highpass', analog = False, output = output))
+        self.get_params(specs)
+        self.save(specs, sig.cheby1(self.N, self.A_pb, self.F_pb,
+                            btype='highpass', analog = False, output = output))
         
     # For BP and BS, A_pb, F_pb and F_stop have two elements each
     def BPman(self, specs):
-        self.save(sig.cheby1(specs['N'], specs['A_pb'],
-                        [specs['F_pb'], specs['F_pb2']], btype='bandpass',
-                        analog = False, output = output))
+        self.get_params(specs)
+        self.save(specs, sig.cheby1(self.N, self.A_pb,[self.F_pb,self.F_pb2],
+                            btype='bandpass', analog = False, output = output))
         
     def BSman(self, specs):
-        self.save(sig.cheby1(specs['N'], specs['A_pb'],
-                [specs['F_pb'], specs['F_pb2']], btype='bandstop', 
-                analog = False, output = output))
+        self.get_params(specs)
+        self.save(specs, sig.cheby1(self.N, self.A_pb, [self.F_pb,self.F_pb2],
+                            btype='bandstop', analog = False, output = output))
 
     # LP: F_pb < F_stop
     def LPmin(self, specs):
-        self.save(sig.iirdesign(specs['F_pb'], specs['F_sb'], 
-                                   specs['A_pb'], specs['A_sb'],
+        self.get_params(specs)
+        self.save(specs, sig.iirdesign(self.F_pb, self.F_sb, self.A_pb, self.A_sb,
                              analog=False, ftype='cheby1', output=output))
    
     # HP: F_stop < F_pb                          
     def HPmin(self, specs):
-        self.save(sig.iirdesign(specs['F_pb'], specs['F_sb'], 
-                                   specs['A_pb'], specs['A_sb'],
+        self.get_params(specs)
+        self.save(specs, sig.iirdesign(self.F_pb, self.F_sb, self.A_pb, self.A_sb,
                              analog=False, ftype='cheby1', output=output))
         
     # BP: F_stop[0] < F_pb[0], F_stop[1] > F_pb[1]    
     def BPmin(self, specs):
-        self.save(sig.iirdesign([specs['F_pb'],specs['F_pb2']], 
-                [specs['F_sb'], specs['F_sb2']], specs['A_pb'], specs['A_sb'],
-                             analog=False, ftype='cheby2', output=output))
+        self.get_params(specs)        
+        self.save(specs, sig.iirdesign([self.F_pb,self.F_pb2], [self.F_sb,self.F_sb2],
+            self.A_pb, self.A_sb, analog=False, ftype='cheby1', output=output))
 
     # BS: F_stop[0] > F_pb[0], F_stop[1] < F_pb[1]            
     def BSmin(self, specs):
-        self.save(sig.iirdesign([specs['F_pb'],specs['F_pb2']], 
-                [specs['F_sb'], specs['F_sb2']], specs['A_pb'], specs['A_sb'],
-                             analog=False, ftype='cheby2', output=output))
+        self.get_params(specs)
+        self.save(specs, sig.iirdesign([self.F_pb,self.F_pb2], [self.F_sb,self.F_sb2],
+            self.A_pb, self.A_sb, analog=False, ftype='cheby1', output=output))
+
