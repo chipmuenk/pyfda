7e34169d9e6865a7359f8a3919d4bfab45cb9feb
chipmuenk <mail@chipmuenk.de>
2015-01-29 21:54:47 +0100

    Major rework of layout and functionality Signed-off-by: chipmuenk <mail@chipmuenk.de>

--- pyFDA/inputWidgets/input_freqs.py
+++ pyFDA/inputWidgets/input_freqs.py
@@ -29,8 +29,6 @@ class InputFreqs(QtGui.QWidget):
         self.DEBUG = DEBUG
         self.specs = specs  # dictionary containing _all_ specifications of the
                             # currently selected filter
-        self.labels = []    # list with labels for combobox
-
         self.qlabel = []    # list with references to QLabel widgets
         self.qlineedit = [] # list with references to QLineEdit widgets
 
@@ -38,71 +36,66 @@ class InputFreqs(QtGui.QWidget):
         
     def initUI(self): 
         self.WVLayout = QtGui.QVBoxLayout() # Widget vertical layout  
-        self.layout   = QtGui.QGridLayout() # sublayout for spec fields
 
         title = "Frequency Specifications"      
-        units = ["Hz", "Normalize 0 to 1", "kHz", "MHz", "GHz"]
         self.unitsf = OrderedDict([
-        ('Normalized', 0.5),
+        ('Normalized to f_S/2', 0.5),
         ('Hz', 1.),
         ('kHz', 1000.),
         ('MHz', 1.e6),
         ('GHz', 1.e9)
         ])
+        
         self.idxOld = 0 # index of comboUnits before last change
         
-        if title != "":
-            bfont = QtGui.QFont()
-            bfont.setBold(True)
+
+        bfont = QtGui.QFont()
+        bfont.setBold(True)
 #            bfont.setWeight(75)
-            self.qtitle = QtGui.QLabel(self) # field for widget title
-            self.qtitle.setText(str(title))
-            self.qtitle.setFont(bfont)
-            self.qtitle.setWordWrap(True)
-            self.WVLayout.addWidget(self.qtitle)
+        self.qtitle = QtGui.QLabel(self) # field for widget title
+        self.qtitle.setText(str(title))
+        self.qtitle.setFont(bfont)
+        self.qtitle.setWordWrap(True)
+        self.WVLayout.addWidget(self.qtitle)    
+
+        self.labelUnits=QtGui.QLabel(self)
+        self.labelUnits.setText("Units")
         
-        if units != []:
-            self.labelUnits=QtGui.QLabel(self)
-            self.labelUnits.setText("Units")
-            
-            self.f_S = self.specs["f_S"]            
-            self.editF_S = QtGui.QLineEdit()
-            self.editF_S.setText(str(self.f_S))
-            self.editF_S.setObjectName("f_S")
-            self.editF_S.editingFinished.connect(self.freqUnits)
+        self.f_S = self.specs["f_S"]            
+        self.editF_S = QtGui.QLineEdit()
+        self.editF_S.setText(str(self.f_S))
+        self.editF_S.setObjectName("f_S")
 
+        self.labelF_S = QtGui.QLabel(self)
+        self.labelF_S.setText("f_S")
 
-            self.comboUnits=QtGui.QComboBox(self)
-            self.comboUnits.setObjectName("comboUnits")
-            self.comboUnits.addItems(self.unitsf.keys())
-            self.comboUnits.setCurrentIndex(self.idxOld)
+        self.comboUnits = QtGui.QComboBox(self)
+        self.comboUnits.setObjectName("comboUnits")
+        self.comboUnits.addItems(self.unitsf.keys())
+        self.comboUnits.setCurrentIndex(self.idxOld)
+        
+        self.butSort = QtGui.QPushButton(self)
+        self.butSort.setText("Sort")
+        self.butSort.setToolTip("Sort frequencies in ascending order.")       
+        self.hbox = QtGui.QHBoxLayout()
+        self.hbox.addWidget(self.comboUnits)
+        self.hbox.addWidget(self.butSort)
+        
+        self.layout = QtGui.QGridLayout() # sublayout for spec fields
+        self.layout.addWidget(self.labelUnits,0,0)
+        self.layout.addLayout(self.hbox,0,1)
+#        self.layout.addWidget(self.butSort,0,2, QtCore.Qt.AlignLeft)
+        self.layout.addWidget(self.labelF_S,1,0)
+        self.layout.addWidget(self.editF_S,1,1)
 
-            self.layout.addWidget(self.labelUnits,0,0)
-            self.layout.addWidget(self.comboUnits,0,1, QtCore.Qt.AlignLeft)
-            self.layout.addWidget(self.editF_S,0,2)
-
-        #self.layout.addWidget(self.qtitle, 0, 0, 2, 1) # span two columns
-
-        # Create a gridLayout consisting of Labels and LineEdit fields
+        # Create a gridLayout consisting of QLabel and QLineEdit fields
         # The number of created lines depends on the number of labels
-        # qlabels is a list with references to the QLabel widgets, 
-        # qlineedit contains references to the QLineEdit widgets
 
         # - Build a list from all entries in the specs dictionary starting 
         #   with "F" (= frequency specifications of the current filter)
         # - Pass the list to setEntries which recreates the widget        
         newLabels = [l for l in self.specs if l[0] == 'F']
         self.setEntries(newLabels = newLabels)
-#        for i in range(len(self.labels)):        
-#           
-#            self.qlabel.append(QtGui.QLabel(self))
-#            self.qlabel[i].setText(self.labels[i])
-#            self.qlineedit.append(QtGui.QLineEdit(str(
-#                                        self.specs[self.labels[i]])))
-#            self.qlineedit[i].editingFinished.connect(self.freqUnits)
-#
-#            self.layout.addWidget(self.qlabel[i],(i+1),0)
-#            self.layout.addWidget(self.qlineedit[i],(i+1),1)
         
         sfFrame = QtGui.QFrame()
         sfFrame.setFrameStyle(QtGui.QFrame.StyledPanel|QtGui.QFrame.Sunken)
@@ -119,8 +112,10 @@ class InputFreqs(QtGui.QWidget):
         
         # SIGNALS & SLOTS
         # Every time a field is edited, call self.freqUnits - the signal is
-        # constructed in _addEntry
+        #   constructed in _addEntry
         self.comboUnits.currentIndexChanged.connect(self.freqUnits)
+        self.editF_S.editingFinished.connect(self.freqUnits)
+        self.butSort.clicked.connect(self._sortEntries)
 
     def mousePressEvent(self, event):
         """
@@ -132,23 +127,26 @@ class InputFreqs(QtGui.QWidget):
 
 #-------------------------------------------------------------        
     def freqUnits(self):
-        
+        """
+        Transform the 
+        """        
         idx = self.comboUnits.currentIndex()
-        freqParams = [l for l in self.specs if l[0] == 'F']
-        self.f_S = float(self.editF_S.text())
-        sender = self.sender()
-#        self.statusBar().showMessage(sender.text() + ' was pressed')
+        freqSpecs = [l for l in self.specs if l[0] == 'F'] # list with freq. specs
+        self.f_S = float(self.editF_S.text()) # sampling frequency
+        sender = self.sender() # origin of signal that triggered the slot
         print(sender.objectName() + ' was edited\n=======================')
+
         if sender.objectName() == "f_S" and self.f_S != 0:
             self.specs['f_S'] = self.f_S
-            for i in range(len(self.labels)):
-#                   print(float(self.qlineedit[i].text()))
+            for i in range(len(self.qlabel)):
                 f = self.specs[self.qlineedit[i].objectName()]
-                self.qlineedit[i].setText(str(f * self.f_S))        
+                self.qlineedit[i].setText(str(f * self.f_S))
 
 #                    self.specs[F] = self.specs[F] / self.unitsf[unit]
         elif sender.objectName() == "comboUnits" and idx != self.idxOld: # unit  has changed
             self.scale = self.comboUnits.itemData(idx)
+            self.editF_S.setVisible(idx > 0) # only visible when not normalized
+            self.labelF_S.setVisible(idx > 0)
             unit = str(self.comboUnits.itemText(idx))
 #            if idx > 0: # not normalized
 #                for F in freqParams:
@@ -158,87 +156,101 @@ class InputFreqs(QtGui.QWidget):
         else: # freq. spec textfield has been changed
             self.storeEntries()
 
-        print(idx, self.f_S, freqParams)
+        print(idx, self.f_S, freqSpecs)
 
         self.idxOld = idx
-  
         
     
-    def setEntries(self, title = "", newLabels = []):
+    def setEntries(self, newLabels = []):
         """
-        Set labels, defaults - when number of elements changes, the 
-        layout has to be rebuilt
-        """
-        if self.DEBUG: print("InputFreqs.Titel:",self.title)
-        if title != "":
-            self.qtitle.setText(title) # new title
-    
+        Set labels and get corresponding values from filter dictionary.
+        When number of elements changes, the layout of subwidget is rebuilt.
+        """ 
         # Check whether the number of entries has changed
-        for i in range(max(len(self.labels), len(newLabels))):
-             # newLabels is shorter than labels -> delete the difference
+        for i in range(max(len(self.qlabel), len(newLabels))):
+             # newLabels is shorter than qlabel -> delete the difference
             if (i > (len(newLabels)-1)):
                 self._delEntry(len(newLabels))
 
-            # newLabels is longer than existing labels -> create new ones!   
-            elif (i > (len(self.labels)-1)):
-                self._addEntry(i,newLabels[i])
+            # newLabels is longer than existing qlabels -> create new ones!   
+            elif (i > (len(self.qlabel)-1)):   
+             self._addEntry(i,newLabels[i])
 
             else:
-                # when label has changed, update it and the default value
-                if (self.labels[i]!=newLabels[i]):     
-                    self.qlabel[i].setText(newLabels[i])
-                    self.labels[i] = newLabels[i]
-                    self.qlineedit[i].setText(str(self.specs[newLabels[i]]))
-                            
+                # when entry has changed, update label and corresponding value
+                if self.qlabel[i].text() != newLabels[i]:     
+                    self.qlabel[i].setText(newLabels[i]) # update label
+
+                    self.qlineedit[i].setText(str(self.specs[newLabels[i]]*self.f_S))
+                    self.qlineedit[i].setObjectName(newLabels[i])  # update ID     
+                     
     def _delEntry(self,i):
         """
-        Element with position i is deleted (qlabel and qlineedit)
+        Delete entry number i from subwidget (QLabel and QLineEdit)
         """
         self.layout.removeWidget(self.qlabel[i])
         self.layout.removeWidget(self.qlineedit[i])
+
         self.qlabel[i].deleteLater()
-        del self.labels[i]
         del self.qlabel[i]
         self.qlineedit[i].deleteLater()
         del self.qlineedit[i]  
         
+
     def _addEntry(self, i, newLabel): 
         """
-        Entry with position i is appended (qlabel und qlineedit)
+        Append entry number i to subwidget (QLabel und QLineEdit)
         """
         self.qlabel.append(QtGui.QLabel(self))
-        self.labels.append(newLabel)
+        self.qlabel[i].setText(newLabel)
+        
         self.qlineedit.append(QtGui.QLineEdit(str(self.specs[newLabel])))
         self.qlineedit[i].editingFinished.connect(self.freqUnits)
         self.qlineedit[i].setObjectName(newLabel)
 
-        self.qlabel[i].setText(newLabel)
-        self.layout.addWidget(self.qlabel[i],(i+1),0)
-        self.layout.addWidget(self.qlineedit[i],(i+1),1)
+        self.layout.addWidget(self.qlabel[i],(i+2),0)
+        self.layout.addWidget(self.qlineedit[i],(i+2),1)
+        
+    def _sortEntries(self): 
+        """
+        Sort spec entries with ascending frequency
+        """
+        fSpecs = []
+        for i in range(len(self.qlineedit)):
+            fSpecs.append(self.qlineedit[i].text())
+#        fSpecs = [f for f in self.qlineedit.text()]        
+        
+        fSpecs.sort()
+        
+        for i in range(len(self.qlineedit)):
+            self.qlineedit[i].setText(fSpecs[i])
+        self.storeEntries()
       
     def loadEntries(self):
         """
-        Reload textfields from global dictionary to update changed weight
-        settings etc.
+        Reload textfields from filter dictionary to update changed settings 
         """
-        for i in range(len(self.labels)):
-            self.qlineedit[i].setText(str(self.specs[self.labels[i]]))
+        for i in range(len(self.qlabels)): 
+            self.qlineedit[i].setText(str(self.specs[self.qlabel[i].text()]))
 
     def storeEntries(self):
         """
-        Store specification entries in dict fb.gD['selFilter']
+        Store specification entries in filter dictionary
         Entries are normalized with sampling frequency self.f_S !
+        The scale factor (khz, ...) is contained neither in f_S nor the specs
+        hence, it cancels out.
         """
-        for i in range(len(self.labels)):
+        for i in range(len(self.qlabel)): 
             self.specs.update(
-                {self.labels[i]:float(self.qlineedit[i].text())/self.f_S})
+                {self.qlabel[i].text():float(self.qlineedit[i].text())/self.f_S})
+
     
 #------------------------------------------------------------------------------ 
     
 if __name__ == '__main__':
     lab = ['F_sb','F_sb','F_sb2']
     app = QtGui.QApplication(sys.argv)
-    form = InputFreqs(labels = lab)#, spec="TEST")
+    form = InputFreqs(specs = fb.gd["curFilter"])#, spec="TEST")
 
     form.setEntries(title = "Gewichte", newLabels = ['W_sb','W_sb2','W_pb','W_pb2'])
     form.setEntries(newLabels = ['W_pb','W_pb2'])
