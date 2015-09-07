aa3a5904362587fcc00a9bd9fb084f2b76c01ac6
chipmuenk <mail@chipmuenk.de>
2015-09-07 15:55:48 +0200

    Corrected path construction for application icon, some PEP-8 improvements Signed-off-by: chipmuenk <mail@chipmuenk.de>

--- pyfda/pyfda.py
+++ pyfda/pyfda.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Mainwindow  for the pyFDA app, initializes UI 
+Mainwindow  for the pyFDA app, initializes UI
 
 Authors: Julia Beike, Christian Muenker and Michael Winkler
 """
@@ -9,7 +9,7 @@ import sys, os
 from PyQt4 import QtGui, QtCore
 
 #import filterbroker as fb # importing filterbroker initializes all its globals
-from .input_widgets import input_tab_widgets 
+from .input_widgets import input_tab_widgets
 from .plot_widgets import plot_tab_widgets
 
 __version__ = "0.1a1"
@@ -17,13 +17,13 @@ __version__ = "0.1a1"
 class pyFDA(QtGui.QMainWindow):
     """
     Create the main window consisting of a tabbed widget for entering filter
-    specifications, poles / zeros etc. and another tabbed widget for plotting 
+    specifications, poles / zeros etc. and another tabbed widget for plotting
     various filter characteristics
-    
-    QMainWindow is used here as it is a class that understands GUI elements like 
+
+    QMainWindow is used here as it is a class that understands GUI elements like
     toolbar, statusbar, central widget, docking areas etc.
     """
-    
+
     def __init__(self):
         self.DEBUG = True
         super(pyFDA, self).__init__()
@@ -41,22 +41,22 @@ class pyFDA(QtGui.QMainWindow):
         - Filter Design button [-> self.startDesignFilt()]
         - Plot Window [-> plotAll.plotAll()]
         """
-        
+
         # Instantiate widget groups
-        self.inputWidgets = input_tab_widgets.InputWidgets() # input widgets        
+        self.inputWidgets = input_tab_widgets.InputWidgets() # input widgets
         self.inputWidgets.setMaximumWidth(320) # comment out for splitter
-        
+
         self.pltWidgets = plot_tab_widgets.PlotWidgets() # plot widgets
-        
+
         # ============== UI Layout =====================================
         _widget = QtGui.QWidget() # this widget contains all subwidget groups
 
         layHMain = QtGui.QHBoxLayout(_widget) # horizontal layout of all groups
 
         # comment out following 3 lines for splitter design
-        layHMain.addWidget(self.inputWidgets)        
+        layHMain.addWidget(self.inputWidgets)
         layHMain.addWidget(self.pltWidgets)
-        layHMain.setContentsMargins(0,0,0,0)#(left, top, right, bottom)
+        layHMain.setContentsMargins(0, 0, 0, 0)#(left, top, right, bottom)
 
 
 # variable size tabs (splitter)
@@ -64,43 +64,43 @@ class pyFDA(QtGui.QMainWindow):
 #        layVInput.addWidget(self.inputWidgets)
 #        layVPlt = QtGui.QVBoxLayout()
 #        layVPlt.addWidget(self.pltWidgets)
-#        
+#
 #        frmInput = QtGui.QFrame()
 #        frmInput.setFrameStyle(QtGui.QFrame.StyledPanel|QtGui.QFrame.Sunken)
 #        frmInput.setLayout(layVInput)
 #        frmInput.setSizePolicy(QtGui.QSizePolicy.Minimum,
 #                                 QtGui.QSizePolicy.Minimum)
-# 
+#
 #        frmPlt = QtGui.QFrame()
 #        frmPlt.setFrameStyle(QtGui.QFrame.StyledPanel|QtGui.QFrame.Sunken)
 #        frmPlt.setLayout(layVPlt)
 #        frmPlt.setSizePolicy(QtGui.QSizePolicy.Minimum,
 #                                 QtGui.QSizePolicy.Minimum)
-#                                 
+#
 #        splitter = QtGui.QSplitter(QtCore.Qt.Horizontal)
 #        splitter.addWidget(frmInput)
 #        splitter.addWidget(frmPlt)
 #        layHMain.addWidget(splitter)
 
         self.setWindowTitle('pyFDA - Python Filter Design and Analysis')
-    
-    
+
+
         # Create scroll area and "monitor" _widget whether scrollbars are needed
         scrollArea = QtGui.QScrollArea()
         scrollArea.setWidget(_widget) # splitter for var. size tabs?
-        
+
         #============= Set behaviour of scroll area ======================
         # scroll bars appear when the scroll area shrinks below this size:
-        scrollArea.setMinimumSize(QtCore.QSize(800, 500)) 
+        scrollArea.setMinimumSize(QtCore.QSize(800, 500))
 #        scrollArea.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAsNeeded) #default
-#        scrollArea.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAsNeeded) # default                                
+#        scrollArea.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAsNeeded) # default
         scrollArea.setSizePolicy(QtGui.QSizePolicy.MinimumExpanding,
                                  QtGui.QSizePolicy.MinimumExpanding)
 
         # Size of monitored widget is allowed to grow:
         scrollArea.setWidgetResizable(True)
 
-        
+
         # CentralWidget (focus of GUI?) is now the ScrollArea
         self.setCentralWidget(scrollArea)
 
@@ -116,7 +116,7 @@ class pyFDA(QtGui.QMainWindow):
 #        fileMenu.addAction(aboutAction)
 
 #        self.statusMessage("Application is initialized.")
-       
+
 
         #----------------------------------------------------------------------
         # SIGNALS & SLOTs
@@ -124,23 +124,26 @@ class pyFDA(QtGui.QMainWindow):
         # Here, signals about spec and design changes from lower hierarchies
         # are distributed. At the moment, only changes in the input widgets are
         # routed to the plot widgets:
-        # 
-        # sigSpecsChanged: signal indicating that filter SPECS have changed, 
+        #
+        # sigSpecsChanged: signal indicating that filter SPECS have changed,
         # requiring partial update of some plot widgets:
         self.inputWidgets.sigSpecsChanged.connect(self.pltWidgets.updateSpecs)
         #
         # sigFilterDesigned: signal indicating that filter has been DESIGNED,
-        #  requiring full update of all plot widgets: 
+        #  requiring full update of all plot widgets:
         self.inputWidgets.sigFilterDesigned.connect(self.pltWidgets.updateData)
 
-        
+
 #        aboutAction.triggered.connect(self.aboutWindow) # open pop-up window
 
 
 #------------------------------------------------------------------------------
     def aboutWindow(self):
+        """
+        Display an "About" window
+        """
         QtGui.QMessageBox.about(self, "About pyFDA",
-        ("(c) 2013 - 15 Christian Münker\n\n"
+                                ("(c) 2013 - 15 Christian Münker\n\n"
         "A graphical tool for designing, analyzing and synthesizing digital filters")
         )
 
@@ -157,34 +160,34 @@ def main():
     """ entry point for the pyfda application """
     app = QtGui.QApplication(sys.argv)
 
-    icon = os.path.join("pyfda","images","icons","Logo_LST_4.svg") 
-    
     _desktop = QtGui.QDesktopWidget()
     screen_h = _desktop.availableGeometry().height()
     screen_w = _desktop.availableGeometry().width()
     print("Available screen resolution:", screen_w, "x", screen_h)
 
-    fontsize = 10    
+    fontsize = 10
     if screen_h < 800:
         delta = 50
     else:
         delta = 100
-
 
     myFont = QtGui.QFont("Tahoma", fontsize)
 
     app.setFont(myFont)
     mainw = pyFDA()
 
+    icon = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                        'images', 'icons', "Logo_LST_4.svg")
+
     app.setWindowIcon(QtGui.QIcon(icon))
     mainw.setWindowIcon(QtGui.QIcon(icon))
-      
+
     # set position + size of main window on desktop
-    mainw.setGeometry(20, 20, screen_w - delta, screen_h - delta) # ltop left / top right, deltax, delta y
+    mainw.setGeometry(20, 20, screen_w - delta, screen_h - delta) # top L / top R, dx, dy
     mainw.show()
 
     sys.exit(app.exec_())
-    
+
 #------------------------------------------------------------------------------
 
 if __name__ == '__main__':
