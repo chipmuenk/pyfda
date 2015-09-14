acd48d757a3a6b5a82c27f61d3934a62c06106df
chipmuenk <mail@chipmuenk.de>
2015-09-14 12:39:13 +0200

    some PEP-8 changes, fixed some bugs to make file testable Signed-off-by: chipmuenk <mail@chipmuenk.de>

--- pyfda/filter_tree_builder.py
+++ pyfda/filter_tree_builder.py
@@ -12,9 +12,9 @@ import pyfda.filterbroker as fb
 
 class FilterTreeBuilder(object):
 
-    def __init__(self, filt_dir, filter_file, comment_char='#', DEBUG=False):
+    def __init__(self, filt_dir, filt_list, comment_char='#', DEBUG=False):
         """
-        - Extract the names of all Python files in 'filt_dir'/'filter_file'
+        - Extract the names of all Python files in 'filt_dir'/'filt_list'
           and write them to a list
         - Try to import all python files and return a dict with all file names
           and corresponding objects (the class needs to have the same name as
@@ -28,7 +28,7 @@ class FilterTreeBuilder(object):
             Name of the subdirectory containing the init-file and the
             Python files to be read, needs to have __init__.py)
 
-        filter_file: string
+        filt_list: string
             Name of the init file
 
         comment_char: char
@@ -38,8 +38,8 @@ class FilterTreeBuilder(object):
             True/False, for printing verbose debug messages
         """
         cwd = os.path.dirname(os.path.abspath(__file__))
-#        self.filt_dir_file = os.path.abspath(cwd + '/' + filt_dir + "/" + filter_file)
-        self.filt_dir_file = os.path.join(cwd, filt_dir, filter_file)
+        self.filt_dir_file = os.path.join(cwd, filt_dir, filt_list)
+        
         if DEBUG:
             print(self.filt_dir_file)
         self.DEBUG = DEBUG
@@ -61,16 +61,16 @@ class FilterTreeBuilder(object):
         This method can also be called when the main app runs to re-read the
         filter directory
         """
-        # Scan filter_file.txt for python file names and extract them
+        # Scan filt_list.txt for python file names and extract them
         self.readFiltFile()
 
         # Try to import all filter modules in filtFileNames, store names and
         # modules in the dict self.design_methods as {filterName:filterModule}:
         self.dynFiltImport()
 
-        # Build a hierarchical dict with all valid filter designs and
-        # response types:
-        fb.fil_tree = self.buildFilTree()
+        # Build a hierarchical dict fb.fil_tree with all valid filter designs
+        # and response types:
+        self.buildFilTree()
 
 #==============================================================================
     def readFiltFile(self):
@@ -80,9 +80,9 @@ class FilterTreeBuilder(object):
           character, whitespace, '.py' and everything after it and returned as
           a list.
         - Lines starting with self.comment_char are stripped of newline,
-          whitespace and comment chars and written to list 'filtFileComments'
+          whitespace and comment chars and written to list 'filt_list_comments'
         - All other lines are discarded (for now)
-        - Collect valid file names (without .py) self.filt_file_names
+        - Collect valid file names (without .py) self.filt_list_names
 
         Parameters
         ----------
@@ -93,10 +93,10 @@ class FilterTreeBuilder(object):
         None
         """
 
-        filtFileComments = []     # comment lines from filter_file
-        # List with design method file names (= class names) in filter_file
+        filt_list_comments = []     # comment lines from filt_list
+        # List with design method file names (= class names) in filt_list
         # (without .py suffix):
-        self.filt_file_names = []
+        self.filt_list_names = []
 
         num_filters = 0           # number of filter design files found
 
@@ -114,8 +114,8 @@ class FilterTreeBuilder(object):
                 if len(curLine) > 1:
                     # Does current line begin with the comment character?
                     if curLine[0] == self.comment_char:
-                        # yes, append line to list filtFileComments :
-                            filtFileComments.append((curLine[1:]))
+                        # yes, append line to list filt_list_comments :
+                            filt_list_comments.append((curLine[1:]))
                     # No, this is not a comment line
                     else:
                         # Is '.py' contained in curLine? Starting at which pos?
@@ -124,7 +124,7 @@ class FilterTreeBuilder(object):
                             # Yes, strip '.py' and all characters after,
                             # append the file name to the lines list,
                             # otherwise discard the line
-                            self.filt_file_names.append(curLine[0:suffixPos])
+                            self.filt_list_names.append(curLine[0:suffixPos])
                             num_filters += 1
 
                 curLine = fp.readline() # read next line
@@ -138,7 +138,7 @@ class FilterTreeBuilder(object):
             if self.DEBUG:
                 print("I/O error({0}): {1}".format(e.errno, e.strerror))
 
-            filtFileComments = self.filt_file_names = []
+            filt_list_comments = self.filt_list_names = []
 
 
 #==============================================================================
@@ -158,14 +158,14 @@ class FilterTreeBuilder(object):
             Subdirectory filt_dir with the Python-Files to import from
             IMPORTANT: filt_dir has to contain an __init__.py File
 
-        fb.gD['imports']
+        self.filt_list_names
             List with the classes to be imported, contained in the
             Python files (ending with .py !!) in pyPackage
 
         Returns
         -------
 
-        imports : dict  containing entries (for SUCCESSFUL imports)
+        self.design_methods: dict  containing entries (for SUCCESSFUL imports)
 
             {
              file name without .py (= class names), e.g. 'cheby1' in
@@ -178,7 +178,7 @@ class FilterTreeBuilder(object):
         self.design_methods = {} # dict with filter name and full module name
         num_imports = 0   # number of successful filter imports
 
-        for pyName in self.filt_file_names:
+        for pyName in self.filt_list_names:
             try:
                 # Try to import the module from the subDirectory (= package)
                 importedModule = __import__('pyfda.' + self.filt_dir + '.' + pyName,
@@ -250,7 +250,7 @@ class FilterTreeBuilder(object):
 
         """
 
-        fil_tree = {}
+        fb.fil_tree = {}
         fb.dm_names = {}
         for dm in self.design_methods:           # iterate over found designMethods(dm)
 
@@ -264,16 +264,16 @@ class FilterTreeBuilder(object):
             ft = cur_filter.ft                  # get filter type ('FIR')
 
             for rt in cur_filter.rt:            # iterate over response types
-                if rt not in fil_tree:           # is rt key in dict already?
-                    fil_tree.update({rt:{}})     # no, create it
+                if rt not in fb.fil_tree:           # is rt key in dict already?
+                    fb.fil_tree.update({rt:{}})     # no, create it
 
-                if ft not in fil_tree[rt]:  # is ft key already in dict[rt]?
-                    fil_tree[rt].update({ft:{}}) # no, create it
-                fil_tree[rt][ft].update({dm:{}}) # append dm to list dict[rt][ft]
+                if ft not in fb.fil_tree[rt]:  # is ft key already in dict[rt]?
+                    fb.fil_tree[rt].update({ft:{}}) # no, create it
+                fb.fil_tree[rt][ft].update({dm:{}}) # append dm to list dict[rt][ft]
                 # finally append all the individual 'min' / 'man' info
-                # to dm in fil_tree. These are e.g. the params for 'min' and /or
+                # to dm in fb.fil_tree. These are e.g. the params for 'min' and /or
                 # 'man' filter order
-                fil_tree[rt][ft][dm].update(cur_filter.rt[rt])
+                fb.fil_tree[rt][ft][dm].update(cur_filter.rt[rt])
 
                 # combine common info for all response types
                 #     com = {'man':{...}, 'min':{...}}
@@ -281,32 +281,31 @@ class FilterTreeBuilder(object):
                 #      e.g. {..., 'LP':{'man':{...}, 'min':{...}}
 
                 for minman in cur_filter.com:
-                    # add info only when 'man' / 'min' exists in fil_tree
-                    if minman in fil_tree[rt][ft][dm]:
+                    # add info only when 'man' / 'min' exists in fb.fil_tree
+                    if minman in fb.fil_tree[rt][ft][dm]:
                         for i in cur_filter.com[minman]:
-                            # Test whether entry exists in fil_tree:
-                            if i in fil_tree[rt][ft][dm][minman]:
+                            # Test whether entry exists in fb.fil_tree:
+                            if i in fb.fil_tree[rt][ft][dm][minman]:
                                 # yes, prepend common data
-                                fil_tree[rt][ft][dm][minman][i] =\
-                                cur_filter.com[minman][i] + fil_tree[rt][ft][dm][minman][i]
+                                fb.fil_tree[rt][ft][dm][minman][i] =\
+                                cur_filter.com[minman][i] + fb.fil_tree[rt][ft][dm][minman][i]
                             else:
                                 # no, create new entry
-                                fil_tree[rt][ft][dm][minman].update(\
+                                fb.fil_tree[rt][ft][dm][minman].update(\
                                                 {i:cur_filter.com[minman][i]})
 
                             if self.DEBUG:
                                 print('\n--- FilterFileReader.buildFilterTree ---')
                                 print(dm, minman, i)
-                                print("fil_tree[minman][i]:",
-                                      fil_tree[rt][ft][dm][minman][i])
+                                print("fb.fil_tree[minman][i]:",
+                                      fb.fil_tree[rt][ft][dm][minman][i])
                                 print("cur_filter.com[minman][i]",
                                   cur_filter.com[minman][i])
 
             del cur_filter # delete obsolete filter object (needed?)
 
-        if self.DEBUG: print("fil_tree = ", fil_tree)
-
-        return fil_tree
+        if self.DEBUG:
+            print("fb.fil_tree = ", fb.fil_tree)
 
 #==============================================================================
     def objectWizzard(self, objectName):
@@ -346,20 +345,20 @@ if __name__ == "__main__":
 
     from PyQt4 import QtGui
     app = QtGui.QApplication(sys.argv)
-#    import filterbroker as fb
+#    import pyfda.filterbroker as fb
     print("===== Initialize FilterReader ====")
 
-    filtFileName = "filter_list.txt"
-    subDirectory = "filter_design"
+    filt_file_name = "filter_list.txt"
+    filt_dir = "filter_design"
     comment_char  = '#'
-    Debug = False
+    DEBUG = False
 
     # Create a new FilterFileReader instance & initialize it
-    myTreeBuilder = FilterTreeBuilder(subDirectory, filtFileName, comment_char, Debug)
+    myTreeBuilder = FilterTreeBuilder(filt_dir, filt_file_name, comment_char, DEBUG)
 
     print("\n===== Start Test ====")
-    for name in fb['imports']:
+    for name in myTreeBuilder.design_methods:
         cur_filter = myTreeBuilder.objectWizzard(name)
         print('cur_filter', cur_filter)
     filterTree = myTreeBuilder.buildFilTree()
-    print('filterTree = ', filterTree)
+    print('fb.fil_tree = ', fb.fil_tree)
