871c7517533bf4aa0fa3f36f31f660a6ee44b75a
chipmuenk <mail@chipmuenk.de>
2015-10-14 09:31:34 +0200

    dm class has been instantiated in set_design_method, don't repeat it here Signed-off-by: chipmuenk <mail@chipmuenk.de>

--- pyfda/input_widgets/input_specs.py
+++ pyfda/input_widgets/input_specs.py
@@ -271,15 +271,15 @@ class InputSpecs(QtGui.QWidget):
             print(type(fb.fil_inst))
  
             # Create / update global instance fb.fil_inst of selected filter dm class
-            err1 = fb.fil_factory.create_fil_inst(fb.fil[0]['dm'])
+            # instantiated in InputFilter.set_design_method
             # call the method specified as a string in the argument of the
-            # filter instance defined previously
-            err2 = fb.fil_factory.call_fil_method(fb.fil[0]['rt'] + fb.fil[0]['fo'])
+            # filter instance defined previously in InputFilter.set_response_type
+            err = fb.fil_factory.call_fil_method(fb.fil[0]['rt'] + fb.fil[0]['fo'])
             # The called method writes coeffs, poles/zeros etc. back to
             # the global filter dict fb.fil[0]
             
-            if err1 > 0  or err2 > 0:
-                raise AttributeError("Unknown design class or method.") 
+            if err > 0:
+                raise AttributeError("Unknown design method.") 
 
             # Update filter order. weights and freq display in case they
             # have been changed by the design algorithm
