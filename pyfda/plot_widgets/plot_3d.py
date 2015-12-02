correct some minor scaling bugs Signed-off-by: chipmuenk <mail@chipmuenk.de>

pyfda/plot_widgets/plot_3d.py
@@ -383,37 +383,40 @@ class Plot3D(QtGui.QMainWindow):
         zlevel_rel = 0.1 # height of plotted zero position relative to zmax
 
 
-        # calculate H(jw)| along the unity circle and |H(z)|, each clipped
-        # between bottom and top
-        if self.chkLog.isChecked():
+        if self.chkLog.isChecked(): # logarithmic scale
             bottom = np.floor(max(self.zmin_dB, H_min_dB) / 10) * 10
             top = self.zmax_dB
-            zlevel = bottom - (top - bottom) * (zlevel_rel)
-            H_UC = H_mag(bb, aa, self.xy_UC, top, H_min=bottom, log=True)
-            Hmag = H_mag(bb, aa, self.z, top, H_min=bottom, log=True)
+            top_bottom = top - bottom
+            
+            zlevel = bottom - top_bottom * zlevel_rel
 
             if self.cmbMode3D.currentText() == 'None': # "Poleposition" for H(f) plot only
                 plevel_top = 2 * bottom - zlevel # height of displayed pole position
                 plevel_btm = bottom
             else:
-                plevel_top = top + (top - bottom) * (plevel_rel - 1)
+                plevel_top = top + top_bottom * (plevel_rel - 1)
                 plevel_btm = top
 
-        else:
+        else: # linear scale
             bottom = max(self.zmin, H_min)
             top = self.zmax
+            top_bottom = top - bottom
         #   top = zmax_rel * H_max # calculate display top from max. of H(f)
-            H_UC = H_mag(bb, aa, self.xy_UC, top, H_min=bottom)
-            Hmag = H_mag(bb, aa, self.z, top, H_min=bottom)
 
-            zlevel = zlevel_rel * top # height of displayed zero position
+            zlevel = bottom + top_bottom * zlevel_rel # height of displayed zero position
 
             if self.cmbMode3D.currentText() == 'None': # "Poleposition" for H(f) plot only
-                plevel_top = H_max * 0.3 # height of displayed pole position
+                plevel_top = bottom + H_max * 0.3 # height of displayed pole position
                 plevel_btm = bottom
             else:
                 plevel_top = plevel_rel * top
                 plevel_btm = top
+                
+        # calculate H(jw)| along the unity circle and |H(z)|, each clipped
+        # between bottom and top
+        H_UC = H_mag(bb, aa, self.xy_UC, top, H_min=bottom, log=self.chkLog.isChecked())
+        Hmag = H_mag(bb, aa, self.z, top, H_min=bottom, log=self.chkLog.isChecked())
+
 
         #===============================================================
         ## plot unit circle