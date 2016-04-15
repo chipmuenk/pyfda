684efd47a237aa37054cf6c350417565ed82d0b9
Chipmuenk <mail@chipmuenk.de>
2016-04-15 17:31:25 +0200

    Call destruct_UI to disconnect signals when changing dynamic widgets Signed-off-by: Chipmuenk <mail@chipmuenk.de>

--- pyfda/input_widgets/input_filter.py
+++ pyfda/input_widgets/input_filter.py
@@ -336,6 +336,7 @@ class InputFilter(QtGui.QWidget):
         fb.fil[0]['dm'] = dm
         if dm != self.dm_last: # dm has changed, create new instance
             #-----
+            fb.fil_inst.destruct_UI() # disconnect signals from dyn. widget
             err = fb.fil_factory.create_fil_inst(dm)
             #-----
             logger.debug("InputFilter.set_design_method triggered: %s" %dm)
