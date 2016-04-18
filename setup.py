ceecd6f1364dbf7e66532fa1395751e43a51c645
chipmuenk <mail@chipmuenk.de>
2016-04-18 21:12:16 +0200

    Add some more project info, change license to MIT Signed-off-by: chipmuenk <mail@chipmuenk.de>

--- setup.py
+++ setup.py
@@ -29,8 +29,35 @@ setup(
     url = 'https://github.com/chipmuenk/pyFDA',
     author = 'Christian Muenker',
     author_email = '',
-    license = 'Apache',
-    # automatically find top-level package and sub-packages input_widgets, 
+    license = 'MIT',
+
+     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Education',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    # automatically find top-level package and sub-packages input_widgets,
     # plot_widgets etc.:
     packages = find_packages(exclude=('contrib', 'docs', 'test')),
     package_data = {'pyfda': ['images/icons/*']},
@@ -47,14 +74,14 @@ setup(
 
 
 """
-On non-Windows platforms (using "setup.py install", "setup.py develop", 
-or by using EasyInstall), a "pyfdax" script will be installed that imports 
+On non-Windows platforms (using "setup.py install", "setup.py develop",
+or by using EasyInstall), a "pyfdax" script will be installed that imports
 "main" from pyfdax.py. main() is called with no arguments, and the
-return value is passed to sys.exit(), so an errorlevel or message to print 
+return value is passed to sys.exit(), so an errorlevel or message to print
 to stderr could be provided (not implemented yet).
 
-On Windows, a set of pyfdax.exe and pyfda_gui.exe launchers are created, 
-alongside a set of pyfdax.py and pyfda_gui.pyw files. The .exe wrappers find 
+On Windows, a set of pyfdax.exe and pyfda_gui.exe launchers are created,
+alongside a set of pyfdax.py and pyfda_gui.pyw files. The .exe wrappers find
 and execute the right version of Python to run the .py or .pyw file.
 """
 # http://locallyoptimal.com/blog/2014/03/14/executable-python-scripts-via-entry-points/
