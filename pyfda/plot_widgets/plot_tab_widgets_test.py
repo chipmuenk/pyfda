25b05ac719847bd9bf8c4e437f7f31c86d3b186c
chipmuenk <mail@chipmuenk.de>
2016-09-18 12:52:08 +0200

    make a simple widget for analysing / demonstrating crash upon exit Signed-off-by: chipmuenk <mail@chipmuenk.de>

--- pyfda/plot_widgets/plot_tab_widgets_test.py
+++ pyfda/plot_widgets/plot_tab_widgets_test.py
@@ -7,45 +7,28 @@ Author: Christian Münker
 from __future__ import print_function, division, unicode_literals, absolute_import
 from PyQt4 import QtGui
 
-from pyfda.plot_widgets import plot_tau_g_test as plot_tau_g
+from pyfda.pyfda_lib import grpdelay
+#from pyfda.plot_widgets import plot_tau_g_test as plot_tau_g
 #from pyfda.plot_widgets import plot_tau_g
-
+import pyfda.filterbroker as fb
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
 #------------------------------------------------------------------------------
 class PlotTabWidgets(QtGui.QWidget):
     def __init__(self, parent):
         super(PlotTabWidgets, self).__init__(parent)
-        QtGui.QWidget.__init__(self)
 
-#        self.pltHf = plot_hf.PlotHf(self)
-#        self.pltPhi = plot_phi.PlotPhi(self)
-#        self.pltPZ = plot_pz.PlotPZ(self)
-
-
-        self.pltTauG = plot_tau_g.PlotTauG(self)
+        self.pltTauG = PlotTauG(self)
         
         
-#        self.pltImpz = plot_impz.PlotImpz(self)
-#        self.plt3D = plot_3d.Plot3D(self)
-
-        self._init_UI()
-
-
-#------------------------------------------------------------------------------
-    def _init_UI(self):
         """ Initialize UI with tabbed subplots """
-        tabWidget = QtGui.QTabWidget()
-        tabWidget.setObjectName("plot_tabs")
-#        tabWidget.addTab(self.pltHf, '|H(f)|')
-#        tabWidget.addTab(self.pltPhi, 'phi(f)')
-#        tabWidget.addTab(self.pltPZ, 'P/Z')
+#        tabWidget = QtGui.QTabWidget()
 
-        tabWidget.addTab(self.pltTauG, 'tau_g')
-        
-#        tabWidget.addTab(self.pltImpz, 'h[n]')
-#        tabWidget.addTab(self.plt3D, '3D')
+#        tabWidget.addTab(self.pltTauG, 'tau_g')
 
         layVMain = QtGui.QVBoxLayout()
-        layVMain.addWidget(tabWidget)
+        layVMain.addWidget(self.pltTauG)        
+#        layVMain.addWidget(tabWidget)
         layVMain.setContentsMargins(1,1,1,1)#(left, top, right, bottom)
 #
         self.setLayout(layVMain)
@@ -57,7 +40,7 @@ class PlotTabWidgets(QtGui.QWidget):
 #        self.pltPhi.draw()
 #        self.pltPZ.draw()
 
-        self.pltTauG.draw()
+        self.pltTauG.draw_taug()
 
 #        self.pltImpz.draw()
 #        self.plt3D.draw()
@@ -68,19 +51,63 @@ class PlotTabWidgets(QtGui.QWidget):
         """ Update plot limits with new filter SPECS and redraw all subplots """
 #        self.pltHf.update_view()
 #        self.pltPhi.update_view()
-        self.pltTauG.update_view()
+        self.pltTauG.draw_taug()
 #        self.pltImpz.update_view()
 
 #        self.pltPZ.draw()
 #        self.plt3D.draw()
         pass
 
+#    def draw_tau(self):
+#        bb = fb.fil[0]['ba'][0]
+#        aa = fb.fil[0]['ba'][1]
+#        
+#        wholeF = fb.fil[0]['freqSpecsRangeType'] != 'half'
+#
+#        [w, tau_g] = grpdelay(bb,aa, 2048, whole = wholeF)#, 
+#
+#        F = w * fb.fil[0]['f_S']
+#
+#        self.ax = self.figure.add_subplot(111)
+#        self.ax.hold(False)
+#        self.ax.plot(F, tau_g)
+#        self.canvas.draw()
+    
+
+class PlotTauG(QtGui.QWidget):
+
+    def __init__(self, parent=None):
+        super(PlotTauG, self).__init__(parent)
+
+        self.figure = Figure(figsize=(10,5))
+        self.resize(800,480)
+        self.canvas = FigureCanvas(self.figure)
+
+        layout = QtGui.QVBoxLayout()
+        layout.addWidget(self.canvas)
+        self.setLayout(layout)
+        self.draw_taug()
+
+    def draw_taug(self):
+        bb = fb.fil[0]['ba'][0]
+        aa = fb.fil[0]['ba'][1]
+
+        [w, tau_g] = grpdelay(bb,aa, 2048, whole = True)#, 
+#            verbose = self.chkWarnings.isChecked())
+
+        F = w * fb.fil[0]['f_S']
+
+        self.ax = self.figure.add_subplot(111)
+        self.ax.hold(False)
+        self.ax.plot(F, tau_g)
+        self.canvas.draw()
+
 #------------------------------------------------------------------------
 
 def main():
     import sys
     app = QtGui.QApplication(sys.argv)
-    form = PlotTabWidgets()
+    form = PlotTabWidgets(None)
     form.show()
     app.exec_()
 
