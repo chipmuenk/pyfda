3a3b3004336c268042e1741f116c8736ce843720
chipmuenk <mail@chipmuenk.de>
2016-10-07 09:50:50 +0200

    import additional elements from QtGui / QtWidgets Signed-off-by: chipmuenk <mail@chipmuenk.de>

--- pyfda/compat.py
+++ pyfda/compat.py
@@ -3,23 +3,24 @@
 try:
     import PyQt5
     from PyQt5 import QtGui, QtCore, Qt
-    from PyQt5.QtWidgets import QWidget
-    from PyQt5.QtWidgets import QFileDialog
-    from PyQt5.QtWidgets import QSizePolicy, QLabel, QInputDialog
-    from PyQt5.QtWidgets import QTabWidget
-    from PyQt5.QtWidgets import QMainWindow, QApplication
-    from PyQt5.QtWidgets import QHBoxLayout, QComboBox
-except Exception as err:
+    from PyQt5.QtGui import QFont  
+    from PyQt5.QtWidgets import QWidget, QComboBox, QLabel, QLineEdit, QFrame
+    from PyQt5.QtWidgets import QPushButton, QCheckBox
+    from PyQt5.QtWidgets import QFileDialog#, QInputDialog
+    from PyQt5.QtWidgets import QSizePolicy
+#    from PyQt5.QtWidgets import 
+    from PyQt5.QtWidgets import QMainWindow, QTabWidget, QApplication
+    from PyQt5.QtWidgets import QHBoxLayout, QVBoxLayout, QGridLayout
+except ImportError as err:
     print(err)
     import PyQt4
-    from PyQt4 import QtGui, QtCore
-    from PyQt4.QtGui import QWidget
-    from PyQt4.QtGui import QFileDialog
-    from PyQt4 import Qt
-    from PyQt4.QtGui import QSizePolicy, QLabel, QInputDialog
-    from PyQt4.QtGui import QTabWidget
-    from PyQt4.QtGui import QMainWindow, QApplication
-    from PyQt4.QtGui import QHBoxLayout, QComboBox
+    from PyQt4 import QtGui, QtCore, Qt
+    from PyQt4.QtGui import QWidget, QComboBox, QLabel, QLineEdit, QFrame
+    from PyQt4.QtGui import QPushButton, QCheckBox, QFont
+    from PyQt4.QtGui import QFileDialog#, QInputDialog
+    from PyQt4.QtGui import QSizePolicy
+    from PyQt4.QtGui import QMainWindow, QTabWidget, QApplication
+    from PyQt4.QtGui import QHBoxLayout, QVBoxLayout, QGridLayout
 
 
 
