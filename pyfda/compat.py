ad65d40c2a563962bc4eb015511f3a9455be4c66
chipmuenk <mail@chipmuenk.de>
2016-10-07 12:09:39 +0200

    reorder entries and add more imports Signed-off-by: chipmuenk <mail@chipmuenk.de>

--- pyfda/compat.py
+++ pyfda/compat.py
@@ -3,24 +3,30 @@
 try:
     import PyQt5
     from PyQt5 import QtGui, QtCore
-    from PyQt5.QtGui import QFont  
-    from PyQt5.QtWidgets import QWidget, QComboBox, QLabel, QLineEdit, QFrame
-    from PyQt5.QtWidgets import QPushButton, QCheckBox
-    from PyQt5.QtWidgets import QFileDialog#, QInputDialog
-    from PyQt5.QtWidgets import QSizePolicy
-#    from PyQt5.QtWidgets import 
-    from PyQt5.QtWidgets import QMainWindow, QTabWidget, QApplication
-    from PyQt5.QtWidgets import QHBoxLayout, QVBoxLayout, QGridLayout
+    from PyQt5.QtCore import pyqtSignal, Qt, QEvent
+    from PyQt5.QtGui import QFont, QTextCursor
+    from PyQt5.QtWidgets import (QWidget, QComboBox, QLabel, QLineEdit, QFrame,
+                                 QPushButton, QCheckBox, QToolButton, QSpinBox,
+                                 QFileDialog, QInputDialog,
+                                 QTableWidget, QTableWidgetItem, QTextBrowser, 
+                                 QSizePolicy, QAbstractItemView,
+                                 QMainWindow, QTabWidget, QApplication,
+                                 QHBoxLayout, QVBoxLayout, QGridLayout)
 except ImportError as err:
     print(err)
     import PyQt4
+
     from PyQt4 import QtGui, QtCore
-    from PyQt4.QtGui import QWidget, QComboBox, QLabel, QLineEdit, QFrame
-    from PyQt4.QtGui import QPushButton, QCheckBox, QFont
-    from PyQt4.QtGui import QFileDialog#, QInputDialog
-    from PyQt4.QtGui import QSizePolicy
-    from PyQt4.QtGui import QMainWindow, QTabWidget, QApplication
-    from PyQt4.QtGui import QHBoxLayout, QVBoxLayout, QGridLayout
+    from PyQt4.QtCore import pyqtSignal, Qt, QEvent
+    from PyQt4.QtGui import (QFont,
+                             QWidget, QComboBox, QLabel, QLineEdit, QFrame,
+                             QPushButton, QCheckBox, QToolButton, QSpinBox,
+                             QFileDialog, QInputDialog,
+                             QTableWidget, QTableWidgetItem, QTextBrowser, QTextCursor,
+                             QSizePolicy, QAbstractItemView,
+                             QMainWindow, QTabWidget, QApplication,
+                             QHBoxLayout, QVBoxLayout, QGridLayout)
+
 
 
 
