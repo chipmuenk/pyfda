f4f3e9679f2c8675b673f93c6bcab9e87c8e4146
Chipmuenk <mail@chipmuenk.de>
2016-11-15 09:43:21 +0100

    Use rt_base from .common filter routines Signed-off-by: Chipmuenk <mail@chipmuenk.de>

--- pyfda/filter_design/butter.py
+++ pyfda/filter_design/butter.py
@@ -20,12 +20,12 @@ Version info:
          
 Author: Christian Muenker
 """
-from __future__ import print_function, division, unicode_literals
+from __future__ import print_function, division, unicode_literals, absolute_import
 import scipy.signal as sig
 from scipy.signal import buttord
-from .common import rt_base
 
 from pyfda.pyfda_lib import fil_save, SOS_AVAIL, lin2unit
+from .common import rt_base
 
 __version__ = "1.4"
 
@@ -59,33 +59,39 @@ class Butter(object):
 
         # common PARAMETERS for all man. / min. filter order response types:
         par_man = ['N', 'F_C'] # manual filter order
-        par_min = [] # minimum filter order
+        par_min = ['N', 'F_C'] # minimum filter order
 
+        self.dicts = ['rtx', 'com']  # additional parameter dicts for rt
         # Common data for all man. / min. filter order response types:
         # This data is merged with the entries for individual response types
         # (common data comes first):
-        self.dicts = ['rt', 'rtx', 'com']
         self.com = {"man":{"vis":vis_man, "dis":dis_man, "msg":msg_man, "par":par_man},
                     "min":{"vis":vis_min, "dis":dis_min, "msg":msg_min, "par":par_min}}
 
         self.ft = 'IIR'
-        self.rt = rt_base
-        self.rtx = {
-          "LP": {"man":{"par":[]}},
-          "HP": {"man":{"par":[]}},
-          "BP": {"man":{"par":['F_C2']}},
-          "BS": {"man":{"par":['F_C2']}},
-                 }
-#        self.rt = {
-#          "LP": {"man":{"par":[]},
-#                 "min":{"par":['F_PB','F_SB']}},
-#          "HP": {"man":{"par":[]},
-#                 "min":{"par":['F_SB','F_PB']}},
-#          "BP": {"man":{"par":['F_C2']},
-#                 "min":{"par":['F_SB','F_PB','F_PB2','F_SB2']}},
-#          "BS": {"man":{"par":['F_C2']},
-#                 "min":{"par":['F_PB','F_SB','F_SB2','F_PB2']}}
+ #       self.rt = rt_base
+#        self.rtx = {
+#          "LP": {"man":{"par":[]}},
+#          "HP": {"man":{"par":[]}},
+#          "BP": {"man":{"par":['F_C2']}},
+#          "BS": {"man":{"par":['F_C2']}},
 #                 }
+        self.rt = {
+          'LP': {'man':{'par':[]},
+                 'min':{'par':[]},
+                 '_targ':{'par':['F_PB','F_SB','A_PB','A_SB']}},
+          'HP': {'man':{'par':[]},
+                 'min':{'par':[]},
+                 '_targ':{'par':['F_SB','F_PB','A_SB','A_PB']}},
+          'BP': {'man':{'par':['F_C2']},
+                 'min':{'par':['F_C2']},
+                 '_targ':{'par':['F_SB', 'F_PB','F_PB2','F_SB2',
+                                 'A_SB', 'A_PB','A_SB2']}},
+          'BS': {'man':{'par':['F_C2']},
+                 'min':{'par':['F_C2']},
+                 '_targ':{'par':['F_PB','F_SB','F_SB2','F_PB2',
+                                 'A_PB','A_SB','A_PB2']}}
+                 }
 
 
         self.info = """
