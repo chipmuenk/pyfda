1e3ba0e903f6ec6564ccff0a81de8dfda6aba58f
Chipmuenk <mail@chipmuenk.de>
2016-11-21 10:46:08 +0100

    Improve error message Signed-off-by: Chipmuenk <mail@chipmuenk.de>

--- pyfda/filter_factory.py
+++ pyfda/filter_factory.py
@@ -208,11 +208,11 @@ class FilterFactory(object):
                 getattr(fil_inst, method)(fil_dict)
                 #------------------------------------------------------------------
             except Exception as e:
-                err_string = "\Error calling {0} of {1}':\n{2}.".format(method, fil_inst, e)
+                err_string = "\nError calling {0} of {1}':\n{2}.".format(method, fil_inst, e)
                 self.err_code = 18
                 
         if self.err_code > 0:
-                logger.error(err_string)
+                logger.error("Err. Code {0}\n".format(self.err_code),err_string)
             
         return self.err_code
         
