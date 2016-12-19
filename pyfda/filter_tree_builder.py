3b9d27f24eb95cf854f2519093b0014456246c50
Chipmuenk <mail@chipmuenk.de>
2016-12-19 17:36:46 +0100

    Remove some trailing spaces Signed-off-by: Chipmuenk <mail@chipmuenk.de>

--- pyfda/filter_tree_builder.py
+++ pyfda/filter_tree_builder.py
@@ -22,24 +22,24 @@ def merge_dicts(d1, d2, path=None, mode='keep1'):
     """
     Merge the multi-level dictionaries d1 and d2. The ``mode`` flag determines the
     behaviour when the same key is present in both dictionaries:
-    
+
     * keep1 : keep the entry from dict1
     * keep2 : keep the entry from dict2
     * add1  : merge the entries, putting the values from dict2 first (important for lists)
     * add2  : merge the entries, putting the values from dict1 first (  "  )
-    
+
     The parameter ``path`` is only used for keeping track of the hierarchical structure
     for error messages, it should not be set when calling the function.
-    
-    dict1 is modified in place and returned, if this is not intended call the 
+
+    dict1 is modified in place and returned, if this is not intended call the
     function using ``new_dict = merge_dicts(dict(d1), d2).
-    
+
     If you need to merge more than two dicts use:
-    
+
     from functools import reduce   # only for py3
     reduce(merge, [d1, d2, d3...]) # add / merge all other dicts into d1
-    
-    Taken with some modifications from: 
+
+    Taken with some modifications from:
     http://stackoverflow.com/questions/7204805/dictionaries-of-dictionaries-merge
     """
     if path is None: path = []
@@ -55,19 +55,19 @@ def merge_dicts(d1, d2, path=None, mode='keep1'):
             else:
                 try:
                     if mode == 'add2':
-                        if (isinstance(d1[key], tuple) and 
-                            isinstance(d2[key], tuple)):  
+                        if (isinstance(d1[key], tuple) and
+                            isinstance(d2[key], tuple)):
                             d1[key] = (d2[key][0], d2[key][1] + d1[key][1])
                         else:
                             d1[key] = d2[key] + d1[key]
-                        
+
                     elif mode == 'add1':
-                        if (isinstance(d1[key], tuple) and 
-                            isinstance(d2[key], tuple)):  
+                        if (isinstance(d1[key], tuple) and
+                            isinstance(d2[key], tuple)):
                             d1[key] = (d1[key][0], d1[key][1] + d2[key][1])
                         else:
                             d1[key] = d1[key] + d2[key]
-                        
+
                     else:
                         logger.warning("Unknown merge mode {0}.".format(mode))
                 except Exception as e:
@@ -122,14 +122,14 @@ class FilterTreeBuilder(object):
         # Scan filter_list.txt for python file names and extract them
         filt_list_names = self.read_filt_file()
 
-        # Try to import all filter modules and classes found in filter_list, 
+        # Try to import all filter modules and classes found in filter_list,
         # store names and modules in the dict fb.fil_classes as {filterName:filterModule}:
         self.dyn_filt_import(filt_list_names)
 
         """
         Read attributes (ft, rt, fo) from all valid filter classes (fc)
         listed in the global dict ``fb.fil_classes`` and store them in a filter
-        tree dict with the hierarchy 
+        tree dict with the hierarchy
                                         rt-ft-fc-fo-subwidget:params.
         """
 
@@ -142,19 +142,18 @@ class FilterTreeBuilder(object):
             if err_code > 0:
                 logger.warning('Skipping filter class "%s" due to import error %d', fc, err_code)
                 continue # continue with next entry in fb.fil_classes
-            
+
             # add attributes from dict to fil_tree for filter class fc
             fil_tree = self.build_fil_tree(fc, ff.fil_inst.rt_dict, fil_tree)
 
+            # merge additional rt_dict (optional) into filter tree
             if hasattr(ff.fil_inst, 'rt_dict_add'):
-                fil_tree_add = {}
                 fil_tree_add = self.build_fil_tree(fc, ff.fil_inst.rt_dict_add)
-                print(pformat(fil_tree_add))                
-                merge_dicts(fil_tree, fil_tree_add, mode='add2')
+                merge_dicts(fil_tree, fil_tree_add, mode='add1')
 
 
-        # Make the dictionary and all sub-dictionaries read-only ("FrozenDict"):       
-        fb.fil_tree = freeze_hierarchical(fil_tree) 
+        # Make the dictionary and all sub-dictionaries read-only ("FrozenDict"):
+        fb.fil_tree = freeze_hierarchical(fil_tree)
 
         # Test Immutatbility
 #        fil_tree_ref = fb.fil_tree['LP']['FIR']['Equiripple']['min']
@@ -223,15 +222,15 @@ class FilterTreeBuilder(object):
 
             logger.info("%d entries found in filter list!\n", num_filters)
             fp.close()
-            
+
             return filt_list_names
-            
+
         except IOError as e:
             logger.critical( 'Filter list file "%s" could not be found.\n\
                 I/O Error(%d): %s' %(self.filt_dir_file, e.errno, e.strerror))
             sys.exit( 'Filter list file "%s" could not be found.\n\
                 I/O Error(%d): %s' %(self.filt_dir_file, e.errno, e.strerror))
-            
+
         except Exception as e:
             logger.error( "Unexpected error: %s", e)
             sys.exit( "Unexpected error: %s", e)
@@ -242,15 +241,15 @@ class FilterTreeBuilder(object):
         Try to import from all filter files found by ``read_filt_file()``,
         auto-detecting available modules / classes:
 
-        - The design classes in a module are specified in the module attribute 
+        - The design classes in a module are specified in the module attribute
           ``filter_classes`` as a dictionary ``{"Cheby":"Chebychev 1"}`` where the
           key is the class name in the module and the value the corresponding name
-          for display. 
-          
+          for display.
+
         - When ``filter_classes`` is a string, use the string
           for both class and combo box name.
-        
-        Filter class, display name and module path are stored in the global 
+
+        Filter class, display name and module path are stored in the global
         dict `fb.fil_classes`.
 
         Parameters
@@ -263,7 +262,7 @@ class FilterTreeBuilder(object):
         Returns
         -------
 
-        None, results are stored in the global dict fb.fil_classes, 
+        None, results are stored in the global dict fb.fil_classes,
         containing entries (for SUCCESSFUL imports) with:
 
             {<class name>:{'name':<display name>, 'mod':<full module name>}
@@ -287,7 +286,7 @@ class FilterTreeBuilder(object):
                     elif isinstance(mod.filter_classes, six.string_types): # String, convert to dict
                         fdict = {mod.filter_classes:mod.filter_classes}
                     else:
-                        logger.warning("Skipping module '%s', its attribute 'filter_classes' has the wrong type '%s'." 
+                        logger.warning("Skipping module '%s', its attribute 'filter_classes' has the wrong type '%s'."
                         %(str(filt_mod), str(type(mod.filter_classes).__name__)))
                         continue # with next entry in filt_list_names
                 else:
@@ -303,25 +302,25 @@ class FilterTreeBuilder(object):
                 continue
             # Now, try to instantiate an instance ff.fil_inst() of filter class fc
             for fc in fdict:
-                if not hasattr(mod, fc): # class k doesn't exist in filter module
+                if not hasattr(mod, fc): # class fc doesn't exist in filter module
                     logger.warning("Skipping filter class '%s', it doesn't exist in module '%s'." %(fc, module_name))
                     continue # continue with next entry in fdict
                 else:
                     fb.fil_classes.update({fc:{'name':fdict[fc],'mod':module_name}})
-                    # when module + class import was successful, add a new entry 
+                    # when module + class import was successful, add a new entry
                     # to the dict with the class name as key and display name and
                     # and fully qualified module path as values, e.g.
                     # 'Butter':{'name':'Butterworth', 'mod':'pyfda.filter_design.butter'}
 
                     num_imports += 1
                     imported_fil_classes += "\t" + filt_mod + "."+ fc + "\n"
-            
+
         if num_imports < 1:
             logger.critical("No filter class could be imported - shutting down.")
             sys.exit("No filter class could be imported - shutting down.")
 
         else:
-            logger.info("Imported successfully the following %d filter classes:\n%s", 
+            logger.info("Imported successfully the following %d filter classes:\n%s",
                     num_imports, imported_fil_classes)
 
 #==============================================================================
@@ -336,23 +335,23 @@ class FilterTreeBuilder(object):
                  'LP': {'man':{'fo':     ('a','N'),
                                'msg':    ('a', r"<br /><b>Note:</b> Read this!"),
                                'fspecs': ('a','F_C'),
-                               'tspecs': ('u', {'frq':('u','F_PB','F_SB'), 
+                               'tspecs': ('u', {'frq':('u','F_PB','F_SB'),
                                                'amp':('u','A_PB','A_SB')})
                               },
                        'min':{'fo':     ('d','N'),
                               'fspecs': ('d','F_C'),
-                              'tspecs': ('a', {'frq':('a','F_PB','F_SB'), 
+                              'tspecs': ('a', {'frq':('a','F_PB','F_SB'),
                                                'amp':('a','A_PB','A_SB')})
                             }
                       },
                 'HP': {'man':{'fo':     ('a','N'),
                               'fspecs': ('a','F_C'),
-                              'tspecs': ('u', {'frq':('u','F_SB','F_PB'), 
+                              'tspecs': ('u', {'frq':('u','F_SB','F_PB'),
                                                'amp':('u','A_SB','A_PB')})
                              },
                        'min':{'fo':     ('d','N'),
                               'fspecs': ('d','F_C'),
-                              'tspecs': ('a', {'frq':('a','F_SB','F_PB'), 
+                              'tspecs': ('a', {'frq':('a','F_SB','F_PB'),
                                                'amp':('a','A_SB','A_PB')})
                              }
                       }
@@ -367,25 +366,25 @@ class FilterTreeBuilder(object):
         for ``cheby1.LPman`` and ``cheby1.LPmin``, listing the parameters to be
         displayed and whether they are active, unused, disabled or invisible for
         each subwidget:
-        
+
         'LP':{
             'IIR':{
                  'Cheby1':{
                      'man':{'fo':     ('a','N'),
                             'msg':    ('a', r"<br /><b>Note:</b> Read this!"),
                             'fspecs': ('a','F_C'),
-                            'tspecs': ('u', {'frq':('u','F_PB','F_SB'), 
+                            'tspecs': ('u', {'frq':('u','F_PB','F_SB'),
                                              'amp':('u','A_PB','A_SB')})
                             },
                      'min':{'fo':     ('d','N'),
                             'fspecs': ('d','F_C'),
-                            'tspecs': ('a', {'frq':('a','F_PB','F_SB'), 
+                            'tspecs': ('a', {'frq':('a','F_PB','F_SB'),
                                              'amp':('a','A_PB','A_SB')})
                             }
                          }
-                   }  
+                   }
              }, ...
-             
+
         Finally, the whole structure is frozen recursively to avoid inadvertedly
         changing the filter tree.
 
@@ -404,7 +403,7 @@ class FilterTreeBuilder(object):
         """
         if not fil_tree:
             fil_tree = {}
-            
+
         ft = ff.fil_inst.ft                  # get filter type (e.g. 'FIR')
 
         for rt in rt_dict:                   # iterate over all response types
@@ -416,7 +415,7 @@ class FilterTreeBuilder(object):
 
             if ft not in fil_tree[rt]:       # filter type already in dict[rt]?
                 fil_tree[rt].update({ft:{}}) # no, create it
-                
+
             if fc not in fil_tree[rt][ft]:       # filter class already in dict[rt][ft]?
                 fil_tree[rt][ft].update({fc:{}}) # no, create it
 
@@ -424,9 +423,9 @@ class FilterTreeBuilder(object):
             fil_tree[rt][ft][fc].update(rt_dict[rt])
 
             if 'COM' in rt_dict:      # Now handle common info
-                for fo in rt_dict[rt]: # iterate over 'min' / 'max' 
+                for fo in rt_dict[rt]: # iterate over 'min' / 'max'
                     if fo in rt_dict['COM']: # and add common info first
-                        merge_dicts(fil_tree[rt][ft][fc][fo], 
+                        merge_dicts(fil_tree[rt][ft][fc][fo],
                                     rt_dict['COM'][fo], mode='add2')
 
         return fil_tree
