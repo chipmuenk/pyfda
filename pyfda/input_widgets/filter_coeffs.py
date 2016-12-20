380e05ac2dc6bc1a3b44d9856998775375e693c7
Chipmuenk <mail@chipmuenk.de>
2016-12-20 17:24:51 +0100

    Clean-up imports Signed-off-by: Chipmuenk <mail@chipmuenk.de>

--- pyfda/input_widgets/filter_coeffs.py
+++ pyfda/input_widgets/filter_coeffs.py
@@ -12,10 +12,9 @@ from pprint import pformat
 import logging
 logger = logging.getLogger(__name__)
 
-from ..compat import (QWidget, QLabel, QLineEdit, QComboBox, QFrame,
-                      QCheckBox, QPushButton,
+from ..compat import (QWidget, QLabel, QLineEdit, QComboBox, QCheckBox, QPushButton,
                       QAbstractItemView, QTableWidget, QTableWidgetItem,
-                      QVBoxLayout, QHBoxLayout, QGridLayout, QSizePolicy,
+                      QVBoxLayout, QHBoxLayout, QSizePolicy,
                       pyqtSignal, QEvent)
 
 import numpy as np
