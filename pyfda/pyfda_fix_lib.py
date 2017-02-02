e928b9a5f4652b1d2166a111d9dd2f05909a48f3
chipmuenk <mail@chipmuenk.de>
2017-02-02 21:39:41 +0100

    add new method csd_digit Signed-off-by: chipmuenk <mail@chipmuenk.de>

--- pyfda/pyfda_fix_lib.py
+++ pyfda/pyfda_fix_lib.py
@@ -8,11 +8,16 @@
 #
 # (c) 2015 Christian Münker 
 #===========================================================================
-from __future__ import division, print_function, unicode_literals # v3line15
+from __future__ import division, print_function, unicode_literals
+
+import logging
+logger = logging.getLogger(__name__)
+
 
 import numpy as np
 #from numpy import (pi, log10, exp, sqrt, sin, cos, tan, angle, arange,
 #                   linspace, array, zeros, ones)
+from numpy import binary_repr
 __version__ = 0.4
 
 #------------------------------------------------------------------------
@@ -373,6 +378,128 @@ class FIX_filt_MA(Fixed):
 #  for k in range(Nx - len(bq)):
 #	for i in len(bq):
 #	  accu_q[k] = fixed(q_acc, (accu_q[k] + fixed(q_mul, x[k+i]*bq[i+1])))
+
+def csdigit(num, WI=0, WF=15):
+    """
+     Returns the canonical signed digit representation of the input number.
+     Useful for simple DSP implementations of multipliers.
+        [digits,pos,neg,err]=csdigit(num,WI,WF)
+    
+     example csdigit(23) returns +0-00-.
+             Where +0-00-. is a representation of +1 in 2**5, -1 in 2**3 and -1 in 2**0 positions.
+             i.e. 23 = 32 - 8 -1
+    
+     example [a,p,n]=csdigit(23.5,6,2) returns
+                a = +0-000.-0
+                p=32
+                n=8.5
+                23.5 = 32 - 8 -.5
+    
+     num   : input number (decimal)
+     WI    : maximum digits to the left of the decimal point
+     WF    : digits to the right of decimal point
+     for example
+        csdigit(.25,2,2) represents xx.xx binary
+        csdigit(.25,0,4) represents .xxxx binary
+     Note: An error is generated if num does not fit inside the representation.
+           It is truncated to the specified resolution WF.
+     Note: A warning message is generated if num doesn't fit in the precision
+           specified. The returned value is truncated.
+     Note: The WI is not used except for a check for overflow and to add
+           leading zeros.
+    
+     Patrick J. Moran
+     AirSprite Technologies Inc.
+    
+     Copyright 2006, Patrick J. Moran for AirSprite Technologies Inc.
+     Redistribution and use in source and binary forms, with or without modification, 
+     are permitted provided that the following conditions are met:
+     
+        1. Redistributions of source code must retain the above copyright notice.
+        2. Any derivative of this work must credit the author, and must be made available to everyone.
+        3. The name of the author may not be used to endorse or promote products derived 
+           from this software without specific prior written permission.
+    
+     THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+     INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+     PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, 
+     INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+     LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+     OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+     CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY 
+     WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+     """
+
+    def findstr(string, char):
+        """ return a list with the indices of all occurences of char in string """
+        idx = []
+        for i in range(len(string)):
+            if string[i] == char:
+                idx.append[i]
+        return idx
+            
+    if not num:
+        help(csdigit)
+        return
+    
+    targetNum=num;
+    num = abs(num)/(2**(-WF));
+    if num != np.floor(num):
+        if np.floor(num) == 0:
+            logger.error('Insufficient precision to represent this number!')
+        else:
+            logger.warning('Some precision has been lost. Returned value is truncated!')
+    
+    num = np.floor(num);
+    if num > 2**(WI+WF):
+        logger.error('Input number is too large for the requested bit representation.');
+    
+    binNum = np.binary_repr(num, WI + WF) # +1? 
+    digits=('0'+0) * np.ones(len(binNum))
+    #%allZeros=allZeros;
+    onesLoc = findstr(binNum, '1')
+    if onesLoc:
+        onesRun=find(diff(onesLoc)==1)
+        while not onesRun:
+            onesPointer=find(diff(onesLoc)==1)+1;
+            addIn=onesLoc(onesPointer(end));
+            adder=('0'+0)*np.ones(len(binNum))
+            adder[addIn]=('1'+0);
+            # keep track of the additional digits
+            digits[addIn]=('-'+0);
+            binAdder=char(adder);
+            binNum=dec2bin(bin2dec(binNum)+bin2dec(binAdder),WI+WF);
+            onesLoc=findstr(binNum,'1');
+            onesRun=find(diff(onesLoc)==1);
+            if len(binNum) > len(digits):
+                digits=['0'+0,digits] # add any additional leading zero
+            
+    pos=bin2dec(binNum)*2**(-WF);
+    negLoc=find(digits==('-'+0));
+    neg=('0'+0) * np.ones(size(binNum));
+    neg[negLoc]='1'+0;
+    neg=bin2dec(char(neg))*2**(-WF);
+    digits[onesLoc]='+'+0;
+    if (len(digits)-WF)>WI:
+        logger.warning('Number overflowed for this CSD representation')
+    
+    if len(digits)<WF:
+        digits=[digits,('0'+0) * np.ones(WF-len(digits))]
+    
+    #digits = char([digits[0:len(digits)-WF], '.'+0, digits[len(digits)-WF+1:-1])
+#    digits = char()
+    if targetNum < 0: # flip representation if negative
+        ntemp=neg
+        neg=pos
+        pos=ntemp
+        digits = strrep(digits,'+','p')
+        digits = strrep(digits,'-','+')
+        digits = strrep(digits,'p','-')
+    
+    # %digits=char(digits);
+    err = targetNum - (pos-neg)
+    
+    return (digits, pos, neg, err)
 
 #######################################
 # If called directly, do some example #
