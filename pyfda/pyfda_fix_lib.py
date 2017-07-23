d90746bf91ee5d5a6c5ce17805917238e399e829
Chipmuenk <mail@chipmuenk.de>
2017-07-23 11:48:46 +0200

    simplify handling of fractional part in CSD conversion Signed-off-by: Chipmuenk <mail@chipmuenk.de>

--- pyfda/pyfda_fix_lib.py
+++ pyfda/pyfda_fix_lib.py
@@ -744,35 +744,32 @@ class Fixed(object):
         elif self.frmt in {'hex', 'bin', 'dec', 'csd'}:
             # quantize & treat overflows of y (float), returning a float
             y_fix = self.fix(y)
-            y_fix_lsb = y_fix * self.LSB
-
-            # fixpoint format, transform to string
-            yi = np.round(np.modf(y_fix_lsb)[1]).astype(int) # integer part
-            yf = np.round(np.modf(y_fix_lsb)[0] * (1 << self.WF)).astype(int) # frac part as integer
+            # represent fixpoint number as integer in the range -2**(W-1) ... 2**(W-1)
+            y_fix_int = int(np.round(y_fix / self.LSB))
+            # split into fractional and integer part, both represented as integer
+            yi = np.round(np.modf(y_fix)[1]).astype(int) # integer part
+            yf = np.round(np.modf(y_fix)[0] * (1 << self.WF)).astype(int) # frac part as integer
 
             # logger.debug("y_fix={0}, yi={1}, yf={2}".format(y_fix, yi, yf))
 
             if self.frmt == 'dec':
-                y_str = str(y_fix_lsb) # use fixpoint number as returned by fix()
+                y_str = str(y_fix) # use fixpoint number as returned by fix()
 
             elif self.frmt == 'hex':
                 if self.WF > 0:
-                    y_str_bin_i = np.binary_repr(y_fix, self.W)[:self.WI+1]
-                    y_str_bin_f = np.binary_repr(y_fix, self.W)[self.WI+1:]
+                    y_str_bin_i = np.binary_repr(y_fix_int, self.W)[:self.WI+1]
+                    y_str_bin_f = np.binary_repr(y_fix_int, self.W)[self.WI+1:]
                     y_str = bin2hex(y_str_bin_i) + "." + bin2hex(y_str_bin_f, frac=True)
                 else:
                     y_str = dec2hex(yi, self.W)
             elif self.frmt == 'bin':
                 # calculate binary representation of fixpoint integer
-                y_str = np.binary_repr(y_fix, self.W)
+                y_str = np.binary_repr(y_fix_int, self.W)
                 if self.WF > 0:
                     # ... and insert the radix point if required
                     y_str = y_str[:self.WI+1] + "." + y_str[self.WI+1:]
             else: # self.frmt = 'csd'
-                if self.WF > 0:
-                    y_str = dec2csd(y_fix_lsb, self.WF) # yes, use fractional bits WF
-                else:
-                    y_str = dec2csd(y_fix, 0) # no, treat as integer
+                y_str = dec2csd(y_fix, self.WF) # convert with WF fractional bits
 
             return y_str
         else:
