43f3e190fe41833f1fd8f77a813b3c20e9b7439a
Chipmuenk <mail@chipmuenk.de>
2017-10-15 17:50:54 +0200

    remove qstr() - it is already defined in pyfda_qt_lib Signed-off-by: Chipmuenk <mail@chipmuenk.de>

--- pyfda/pyfda_lib.py
+++ pyfda/pyfda_lib.py
@@ -274,28 +274,6 @@ def extract_file_ext(file_type):
     return [t.strip('(*)') for t in ext_list] # remove '(*)'
 
 #------------------------------------------------------------------------------
-def qstr(data):
-    """
-    Convert object (QVariant, QSTring, string, number) to string.
-
-    In Python 3, python Qt objects are automatically converted to QVariant
-    when stored as "data" e.g. in a QComboBox and converted back when
-    retrieving. In Python 2, QVariant is returned when itemData is retrieved.
-    This is first converted from the QVariant container format to a
-    QString, next to a "normal" non-unicode string.
-
-    Returns:
-
-    The current text / QVariant data as a string
-    """
-    if "QVariant" in str(type(data)):
-        # Python 2: convert QVariant -> QString -> str
-        string = str(data.toString())
-    else:
-        # `data` is of type str already, is numeric or is a QString (Py3)
-        string = str(data)
-    return string
-
 
 def dB(lin, power = False):
     """
