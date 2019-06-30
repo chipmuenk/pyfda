7ef37e5bf49dbf39ea62decc01a6803aacdc9fda
chipmuenk <mail@chipmuenk.de>
2019-06-30 21:43:16 +0200

    fix if ... elif bug Signed-off-by: chipmuenk <mail@chipmuenk.de>

--- pyfda/plot_widgets/plot_impz.py
+++ pyfda/plot_widgets/plot_impz.py
@@ -194,7 +194,7 @@ class Plot_Impz(QWidget):
                     logger.error('Unknown "fx_sim" command option "{0}"\n'\
                                  '\treceived from "{1}"'.format(dict_sig['fx_sim'],dict_sig['sender']))                   
 
-            if 'specs_changed' in dict_sig or 'view_changed' in dict_sig or self.needs_draw:
+            elif 'specs_changed' in dict_sig or 'view_changed' in dict_sig or self.needs_draw:
                 self.impz()
 
             elif 'data_changed' in dict_sig or\
