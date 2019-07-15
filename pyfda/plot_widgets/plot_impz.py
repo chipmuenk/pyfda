a904c6a6be4dac6593c6d377ba5b128ce370efc9
chipmuenk <mail@chipmuenk.de>
2019-07-15 17:30:39 +0200

    debug visible / invisible behaviour for fixpoint sim Signed-off-by: chipmuenk <mail@chipmuenk.de>

--- pyfda/plot_widgets/plot_impz.py
+++ pyfda/plot_widgets/plot_impz.py
@@ -177,63 +177,62 @@ class Plot_Impz(QWidget):
             logger.warning("Stopped infinite loop:\n{0}".format(pprint_log(dict_sig)))
             return
 
-        if self.isVisible():
-            if 'fx_sim' in dict_sig:
-                if dict_sig['fx_sim'] == 'specs_changed':
-                    self.needs_calc = True
-                    qstyle_widget(self.ui.but_run, "changed")
+        if 'fx_sim' in dict_sig:
+            if dict_sig['fx_sim'] == 'specs_changed':
+                self.needs_calc = True
+                self.ui.update_N(dict_sig) # needed?
+                qstyle_widget(self.ui.but_run, "changed")
+                if self.isVisible():
+                    self.impz()
 
-                elif dict_sig['fx_sim'] == 'get_stimulus':
-                    """
-                    - Select Fixpoint mode
+            elif dict_sig['fx_sim'] == 'get_stimulus':
+                """
+                - Select Fixpoint mode
 
-                    - Start fixpoint simulation using `self.fx_run()` where the stimulus
-                      is calculated, quantized and passed to dict_sig with `'fx_sim':'set_stimulus'`
-                      and `'fx_stimulus':<quantized stimulus>`. Stimuli are scaled with the input
-                      fractional word length, i.e. with 2**WF (input) to obtain integer values
+                - Start fixpoint simulation using `self.fx_run()` where the stimulus
+                  is calculated, quantized and passed to dict_sig with `'fx_sim':'set_stimulus'`
+                  and `'fx_stimulus':<quantized stimulus>`. Stimuli are scaled with the input
+                  fractional word length, i.e. with 2**WF (input) to obtain integer values
 
-                    # TODO: correct?
-
-                      """
-                    self.needs_calc = True # always require recalculation when triggered externally
-                    qstyle_widget(self.ui.but_run, "changed")
-                    self.fx_select("Fixpoint")
+                # TODO: correct?
+                  """
+                self.needs_calc = True # always require recalculation when triggered externally
+                qstyle_widget(self.ui.but_run, "changed")
+                self.fx_select("Fixpoint")
+                if self.isVisible():
                     self.fx_run(dict_sig=None, set_stimuli=True)
 
-                elif dict_sig['fx_sim'] == 'set_results':
-                    """
-                    - Convert simulation results to integer and transfer them to the plotting
-                      routine
-                    """
+            elif dict_sig['fx_sim'] == 'set_results':
+                """
+                - Convert simulation results to integer and transfer them to the plotting
+                  routine
+                """
+                logger.info("Received fixpoint results.")
+                self.fx_run(dict_sig, set_stimuli=False)
 
-                    logger.info("Received fixpoint results.")
-                    self.fx_run(dict_sig, set_stimuli=False)
+            elif dict_sig['fx_sim'] == 'error':
+                self.needs_calc = True
+                qstyle_widget(self.ui.but_run, "error")
 
-                elif dict_sig['fx_sim'] == 'error':
-                    logger.error('Fixpoint simulation error\n{0}'.format(pprint_log(dict_sig)))
-                    self.needs_calc = True
-                    qstyle_widget(self.ui.but_run, "error")
+            elif not dict_sig['fx_sim']:
+                logger.error('Missing option for "fx_sim".')
 
-                elif not dict_sig['fx_sim']:
-                    logger.error('Missing option for "fx_sim".')
+            else:
+                logger.error('Unknown "fx_sim" command option "{0}"\n'\
+                             '\treceived from "{1}"'.format(dict_sig['fx_sim'],
+                                               dict_sig['sender']))
 
-                else:
-                    logger.error('Unknown "fx_sim" command option "{0}"\n'\
-                                 '\treceived from "{1}"'.format(dict_sig['fx_sim'],
-                                                   dict_sig['sender']))
+        if self.isVisible():
 
-            elif 'view_changed' in dict_sig:
+            if 'view_changed' in dict_sig:
                 self.impz()
 
-            elif 'data_changed' in dict_sig or 'specs_changed' in dict_sig or \
-                    ('fx_sim' in dict_sig and dict_sig['fx_sim'] == 'specs_changed')\
-                    or self.needs_calc:
+            elif 'data_changed' in dict_sig or 'specs_changed' in dict_sig or self.needs_calc:
 
                 self.ui.update_N(dict_sig)
 
                 self.needs_calc = True
                 qstyle_widget(self.ui.but_run, "changed")
-                #if 'ui' in dict_sig['sender']:
                 self.impz()
 
             elif 'home' in dict_sig:
@@ -248,16 +247,15 @@ class Plot_Impz(QWidget):
                 self.redraw() # redraw current widget
 
         else: # invisible
-            if 'data_changed' in dict_sig or 'specs_changed' in dict_sig or\
-                    ('fx_sim' in dict_sig and dict_sig['fx_sim'] == 'specs_changed'):
+            if 'data_changed' in dict_sig or 'specs_changed' in dict_sig:
                 self.needs_calc = True
             elif 'ui_changed' in dict_sig and dict_sig['ui_changed'] == 'resized':
                 self.needs_redraw[:] = [True] * 2
 
-            elif 'fx_sim' in dict_sig and dict_sig['fx_sim'] == 'get_stimulus':
-                    self.needs_calc = True # always require recalculation when triggered externally
-                    qstyle_widget(self.ui.but_run, "changed")
-                    self.fx_select("Fixpoint")
+#            elif 'fx_sim' in dict_sig and dict_sig['fx_sim'] == 'get_stimulus':
+#                    self.needs_calc = True # always require recalculation when triggered externally
+#                    qstyle_widget(self.ui.but_run, "changed")
+#                    self.fx_select("Fixpoint")
 
         if propagate:
             # signals of local subwidgets are propagated,
@@ -317,6 +315,8 @@ class Plot_Impz(QWidget):
             self.needs_redraw[self.tabWidget.currentIndex()] = False
 
         qstyle_widget(self.ui.but_run, "normal")
+        if self.fx_sim:
+            self.sig_tx.emit({'sender':__name__, 'fx_sim':'finish'})
 
 # =============================================================================
 
