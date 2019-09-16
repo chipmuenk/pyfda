ced794d1b88255ccdad0d590d1d5bcb966f5e8ef
chipmuenk <mail@chipmuenk.de>
2019-09-16 09:13:20 +0200

    improve documentation Signed-off-by: chipmuenk <mail@chipmuenk.de>

--- pyfda/fixpoint_widgets/fixpoint_helpers.py
+++ pyfda/fixpoint_widgets/fixpoint_helpers.py
@@ -30,51 +30,71 @@ from pyfda.pyfda_lib import qstr, safe_eval, to_html
 def rescale(mod, sig_i, QI, QO):
     """
     Change word length of input signal `sig_i` to `WO` bits, using the 
-    quantization and saturation methods specified by QO[`quant`] and QO[`ovfl`].
+    quantization and saturation methods specified by ``QO['quant']`` and 
+    ``QO['ovfl']``.
     
     Parameters
     ----------
     
-    mod: instance of migen Module
+    mod: Module (migen)
+        instance of migen module
     
     sig_i: Signal (migen)
+        Signal to be requantized
     
-    QI: quantization dict for input word
+    QI: dict
+        Quantization dict for input word, only the keys 'WI' and 'WF' for integer
+        and fractional wordlength are evaluated. QI['WI'] = 2 and QI['WF'] = 13
+        e.g. define Q-Format '2.13'  with 2 integer, 13 fractional bits and 1 implied 
+        sign bit = 16 bits total.
     
     QO: dict
-        fixpoint format for input resp. output word, specified as  Q-format, e.g.
-        '2.13' (2 integer, 14 fractional bits, 1 implied sign bit = 16 bits total)
+        Quantization dict for output word format; the keys 'WI' and 'WF' for 
+        integer and fractional wordlength are evaluated as well as the keys 'quant'
+        and 'ovfl' describing requantization and overflow behaviour.
     
-    #### Input and output word are aligned at their binary points. ####
+    **Input and output word are aligned at their binary points.**
     
+    The following shows an example of rescaling an input word from Q2.4 to Q0.3
+    using wrap-around and truncation. It's easy to see that for simple wrap-around
+    logic, the sign of the result may change.
+    
+    ::
+
       S | WI1 | WI0 * WF0 | WF1 | WF2 | WF3  :  WI = 2, WF = 4, W = 7
-      0 |  1  |  0  *  1  |  0  |  1  |  1   =  43 (dec), 43/16 = 2 + 11/16 (float)
+      0 |  1  |  0  *  1  |  0  |  1  |  1   =  43 (dec) or 43/16 = 2 + 11/16 (float)
                     *
               |  S  * WF0 | WF1 | WF2        :  WI = 0, WF = 3, W = 4
-                 0  *  1  |  0  |  1         =  7 (dec), 7/8 (float)
+                 0  *  1  |  0  |  1         =  7 (dec) or 7/8 (float)
 
               
-    The real (world) value is obtained by multiplying the integer value by 2 ** (-WF).
+    The float or "real (world) value" is calculated by multiplying the integer 
+    value by 2 ** (-WF).
     
     For requantizing two numbers to the same WI and WF, imagine both binary numbers
-    to be right-aligned.
+    to be right-aligned. Changes in the number of integer bits `dWI` and fractional
+    bits `dWF` are handled separately.
     
-    For reducing the number of fractional bits by `dWF`, simply right-shift the
-    integer number by `dWF`. For rounding, add '1' to the bit below the truncation
-    point before right-shifting. 
+    Fractional Bits:
     
-    Extend the number of fractional bits by left-shifting the integer by `dWF`.
+    - For reducing the number of fractional bits by `dWF`, simply right-shift the
+      integer number by `dWF`. For rounding, add '1' to the bit below the truncation
+      point before right-shifting. 
     
-    For reducing the number of integer bits by `dWI`, simply right-shift the
-    integer by `dWI`.
+    - Extend the number of fractional bits by left-shifting the integer by `dWF`,
+      LSB's are filled with zeros.
+      
+    Integer Bits:
     
-    The number of fractional bits is SIGN-EXTENDED by filling up the left-most
-    bits with the sign bit.
+    - For reducing the number of integer bits by `dWI`, simply right-shift the
+      integer by `dWI`.
+    
+    - The number of fractional bits is SIGN-EXTENDED by filling up the left-most
+      bits with the sign bit.
     
     """
     WI_I = QI['WI']
     WI_F = QI['WF']
-    WI   = QI['W']
     #logger.warning("WI:{0}:{1}".format(WI, type(WI)))
 
     WO_I = QO['WI']
