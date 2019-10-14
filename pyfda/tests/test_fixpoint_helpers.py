8a4e71a354bedc982bc5e6dc2d740a42a9ee7284
Christian Münker <mail@chipmuenk.de>
2019-10-14 17:16:31 +0200

    implent a migen DUT and starting testing rescale Signed-off-by: Christian Münker <mail@chipmuenk.de>

--- pyfda/tests/test_fixpoint_helpers.py
+++ pyfda/tests/test_fixpoint_helpers.py
@@ -13,12 +13,21 @@ Test suite for fixpoint_helpers.py
 import unittest
 import numpy as np
 from pyfda import pyfda_fix_lib as fix_lib
-from pyfda.fixpoint_widgets.fixpoint_helpers import rescale
+try:
+    from migen import Cat, If, Replicate, Signal, Module, run_simulation
+    from pyfda.fixpoint_widgets.fixpoint_helpers import rescale
+    HAS_MIGEN = True
+except ImportError:
+    HAS_MIGEN = False
 
 
 class TestSequenceFunctions(unittest.TestCase):
 
     def setUp(self):
+        self.p_in =  {'WI':0, 'WF':15, 'W':16, 'ovfl':'wrap', 'quant':'round'}
+        self.p_out = {'WI':0, 'WF':15, 'W':16, 'ovfl':'wrap', 'quant':'round'}
+        if HAS_MIGEN:
+            self.dut = DUT(self.p_in, self.p_out)
         q_obj = {'WI':0, 'WF':3, 'ovfl':'sat', 'quant':'round', 'frmt': 'dec', 'scale': 1}
         self.myQ = fix_lib.Fixed(q_obj) # instantiate fixpoint object with settings above
 
@@ -26,6 +35,33 @@ class TestSequenceFunctions(unittest.TestCase):
         self.y_list_cmplx = [-1.1j + 0.1, -1.0 - 0.3j, -0.5-0.5j, 0j, 0.5j, 0.9, 0.99+0.3j, 1j, 1.1]
         # list with various invalid strings
         self.y_list_validate = ['1.1.1', 'xxx', '123', '1.23', '', 1.23j + 3.21, '3.21 + 1.23 j']
+
+        self.fix_in = [0,1,15,32767,-1,-64,0] # last zero isn't tested due to latency of 1
+        self.fix_out = [0,1,15,32767,-1,-64]
+
+    def tb_dut(self, stimulus, inputs, outputs):
+        """ use stimulus list from widget as input to filter """
+        for x in stimulus:
+            yield self.dut.i.eq(int(x)) # pass one stimulus value to filter
+            inputs.append(x) # and append it to input list
+            outputs.append((yield self.dut.o)) # append filter output to output list
+            yield # ??
+
+#------------------------------------------------------------------------------
+    def run_sim(self, stimulus):
+        """
+        Pass stimuli and run filter simulation, see
+        https://reconfig.io/2018/05/hello_world_migen
+        https://github.com/m-labs/migen/blob/master/examples/sim/fir.py
+        """
+        inputs = []
+        response = []
+
+        testbench = self.tb_dut(stimulus, inputs, response)
+
+        run_simulation(self.dut, testbench)
+
+        return response
 
 #
 #    def test_shuffle(self):
@@ -186,15 +222,15 @@ class TestSequenceFunctions(unittest.TestCase):
 
     def test_fix_wrap(self):
         """
-        Test wrap around
+        Test wrap around -np.inf, , np.inf ,np.nan, , np.nan
         """
-        y_list_ovfl = [-np.inf, -3.2, -2.2, -1.2, -1.0, -0.5, 0, 0.5, 0.8, 1.0, 1.2, 2.2, 3.2, np.inf]
+        y_list_ovfl = [-3.2, -2.2, -1.2, -1.0, -0.5, 0, 0.5, 0.8, 1.0, 1.2, 2.2, 3.2]
 
         # Integer representation, wrap
         q_obj = {'WI':3, 'WF':0, 'ovfl':'wrap', 'quant':'round', 'frmt': 'dec', 'scale': 8}
         self.myQ.setQobj(q_obj)
         yq_list = self.myQ.fixp(y_list_ovfl)
-        yq_list_goal = [np.nan, 6.0, -2.0, 6.0, -8.0, -4.0, 0.0, 4.0, 6.0, -8.0, -6.0, 2.0, -6.0, np.nan]
+        yq_list_goal = [6.0, -2.0, 6.0, -8.0, -4.0, 0.0, 4.0, 6.0, -8.0, -6.0, 2.0, -6.0]
         np.testing.assert_array_equal(yq_list, yq_list_goal)
 
         # wrap around behaviour / floor quantization
@@ -242,6 +278,49 @@ class TestSequenceFunctions(unittest.TestCase):
         yq_list = list(self.myQ.float2frmt(self.y_list))
         self.assertEqual(yq_list, yq_list_goal)
 
+    #==============================================================================
+    #   Test rescale routine, this needs a migen class (DUT)
+    #   The input to the run_sim methods needs to be an iterable, output is a list of integer
+    # due to latency of one, strip last element of input and first of output
+
+
+    def test_fix_id(self):
+        """
+        test identity of input integer list and output list, passed thru migen 
+        instance
+        """
+        response = self.run_sim(self.fix_in[:])
+        self.assertEqual(self.fix_in[:-1], response[1:])
+
+    def test_fix_trunc_wf(self):
+        """
+        Test rescaling when fractional word is shortened by 4 bits
+        """
+        self.p_in =  {'WI':0, 'WF':19, 'W':20, 'ovfl':'wrap', 'quant':'round'}
+        self.p_out = {'WI':0, 'WF':15, 'W':16, 'ovfl':'wrap', 'quant':'round'}
+        q_out = self.p_out.copy()
+        q_out.update({'WI':15, 'WF':0, 'W':16, 'scale':1/16})
+        self.dut = DUT(self.p_in, self.p_out)
+
+        # Integer representation, wrap
+        self.myQ.setQobj(q_out)
+        fixq_in = list(self.myQ.fixp(self.fix_in))
+
+        response = self.run_sim(self.fix_in[:])
+        #self.assertEqual(self.fix_in[:-1], response[1:])
+        self.assertEqual(fixq_in[:-1], response[1:])
+
+###############################################################################
+# migen class for testing rescale operation
+class DUT(Module):
+    def __init__(self, par_in, par_out):
+
+        # ------------- Define I/Os -------------------------------------------
+        self.i = Signal((par_in['W'], True)) # input signal
+        self.o = Signal((par_out['W'], True)) # output signal
+        ###
+        # rescale from input format to output format
+        self.comb += self.o.eq(rescale(self, self.i, par_in, par_out))
 
 if __name__=='__main__':
     unittest.main()
