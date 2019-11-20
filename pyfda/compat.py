78d19fdb2ee93e239b4246fbbfc9d39608c90cfd
chipmuenk <mail@chipmuenk.de>
2019-11-20 11:30:08 +0100

    remove references to PyQt4 as this might cause strange behaviour instead of a clean crash when PyQt5 is missing Signed-off-by: chipmuenk <mail@chipmuenk.de>

--- pyfda/compat.py
+++ pyfda/compat.py
@@ -7,52 +7,50 @@
 # (see file LICENSE in root directory for details)
 
 """
-Compatibility wrapper to obtain same syntax for both Qt4 and 5
+Was: Compatibility wrapper to obtain same syntax for both Qt4 and 5, PyQt4 has 
+been removed
 """
 
-try:
-    import PyQt5
-    from PyQt5 import QtGui, QtCore, QtTest
-    from PyQt5.QtCore import (Qt, QEvent, QT_VERSION_STR, QSize, QSysInfo, 
-                              QObject, QVariant, pyqtSignal, pyqtSlot)
-    from PyQt5.QtGui import (QFont, QFontMetrics, QIcon, QImage, QTextCursor, QColor, 
-                                QBrush, QPalette, QPixmap)
-    from PyQt5.QtWidgets import (QAction, QMenu, 
-                                 QMainWindow, QTabWidget, QApplication, QRadioButton,
-                                 QScrollArea, QSplitter, QMessageBox, QDialog,
-                                 QWidget, QComboBox, QLabel, QLineEdit, QFrame,
-                                 QPushButton, QCheckBox, QToolButton, QSpinBox, QDial,
-                                 QFileDialog, QInputDialog, QPlainTextEdit,
-                                 QTableWidget, QTableWidgetItem, QTextBrowser,
-                                 QSizePolicy, QAbstractItemView,
-                                 QHBoxLayout, QVBoxLayout, QGridLayout,
-                                 QStyledItemDelegate, QStyle)
-    from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-    from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
-    HAS_QT5 = True
-
-except ImportError:
-    import PyQt4
-
-    from PyQt4 import QtGui, QtCore, QtTest
-    from PyQt4.QtCore import (Qt, QEvent, QT_VERSION_STR, QSize, QSysInfo,
-                              QObject, QVariant, pyqtSignal, pyqtSlot)
-    from PyQt4.QtGui import (QAction, QMenu, 
-                             QFont, QFontMetrics, QIcon, QImage, QColor, QBrush, QStyle,
-                             QPalette, QPixmap, 
+import PyQt5
+from PyQt5 import QtGui, QtCore, QtTest
+from PyQt5.QtCore import (Qt, QEvent, QT_VERSION_STR, QSize, QSysInfo, 
+                          QObject, QVariant, pyqtSignal, pyqtSlot)
+from PyQt5.QtGui import (QFont, QFontMetrics, QIcon, QImage, QTextCursor, QColor, 
+                            QBrush, QPalette, QPixmap)
+from PyQt5.QtWidgets import (QAction, QMenu, 
                              QMainWindow, QTabWidget, QApplication, QRadioButton,
                              QScrollArea, QSplitter, QMessageBox, QDialog,
                              QWidget, QComboBox, QLabel, QLineEdit, QFrame,
                              QPushButton, QCheckBox, QToolButton, QSpinBox, QDial,
                              QFileDialog, QInputDialog, QPlainTextEdit,
-                             QTableWidget, QTableWidgetItem, QTextBrowser, QTextCursor,
+                             QTableWidget, QTableWidgetItem, QTextBrowser,
                              QSizePolicy, QAbstractItemView,
                              QHBoxLayout, QVBoxLayout, QGridLayout,
-                             QStyledItemDelegate)
+                             QStyledItemDelegate, QStyle)
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 
-    from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
-    from matplotlib.backends.backend_qt4agg import NavigationToolbar2QT as NavigationToolbar
-    HAS_QT5 = False
+#except ImportError:
+#    import PyQt4
+#
+#    from PyQt4 import QtGui, QtCore, QtTest
+#    from PyQt4.QtCore import (Qt, QEvent, QT_VERSION_STR, QSize, QSysInfo,
+#                              QObject, QVariant, pyqtSignal, pyqtSlot)
+#    from PyQt4.QtGui import (QAction, QMenu, 
+#                             QFont, QFontMetrics, QIcon, QImage, QColor, QBrush, QStyle,
+#                             QPalette, QPixmap, 
+#                             QMainWindow, QTabWidget, QApplication, QRadioButton,
+#                             QScrollArea, QSplitter, QMessageBox, QDialog,
+#                             QWidget, QComboBox, QLabel, QLineEdit, QFrame,
+#                             QPushButton, QCheckBox, QToolButton, QSpinBox, QDial,
+#                             QFileDialog, QInputDialog, QPlainTextEdit,
+#                             QTableWidget, QTableWidgetItem, QTextBrowser, QTextCursor,
+#                             QSizePolicy, QAbstractItemView,
+#                             QHBoxLayout, QVBoxLayout, QGridLayout,
+#                             QStyledItemDelegate)
+#
+#    from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
+#    from matplotlib.backends.backend_qt4agg import NavigationToolbar2QT as NavigationToolbar
     
 #==============================================================================
 
@@ -65,22 +63,13 @@ class QFD(QFileDialog):
         super(QFD, self).__init__(parent)
 
     def getOpenFileName_(self, **kwarg):
-        if HAS_QT5:
-            return self.getOpenFileName(**kwarg)
-        else:
-            return self.getOpenFileNameAndFilter(**kwarg)
+        return self.getOpenFileName(**kwarg)
 
     def getOpenFileNames_(self, **kwarg):
-        if HAS_QT5:
-            return self.getOpenFileNames(**kwarg)
-        else:
-            return self.getOpenFileNamesAndFilter(**kwarg)
+        return self.getOpenFileNames(**kwarg)
 
     def getSaveFileName_(self, **kwarg):
-        if HAS_QT5:
-            return self.getSaveFileName(**kwarg)
-        else:
-            return self.getSaveFileNameAndFilter(**kwarg)
+        return self.getSaveFileName(**kwarg)
 
 
 if __name__ == '__main__':
