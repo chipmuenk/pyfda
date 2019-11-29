3e8b1d38d8a309a05ef9eb8491a79be5005d1104
Christian Münker <mail@chipmuenk.de>
2019-11-29 15:19:18 +0100

    eliminate direct comparison of windows names Signed-off-by: Christian Münker <mail@chipmuenk.de>

--- pyfda/pyfda_fft_windows.py
+++ pyfda/pyfda_fft_windows.py
@@ -85,6 +85,9 @@ windows =\
 
 def calc_window_function(win_dict, win_name):
     
+    par = []
+    info = ""
+    
     if win_name not in windows:
         logger.warning("Unknown window name {}, using rectangular window instead.".format(win_name))
         win_name = "Boxcar"
@@ -93,40 +96,14 @@ def calc_window_function(win_dict, win_name):
         fn_name = win_name.lower()
     else:
         fn_name = d['fn_name']
-    if 'par' not in d:
-        par = []
-    if 'info' not in d:
-        info = ""
-    else:
+    if 'par' in d:
+        par = d['par']
+    if 'info' in d:
         info = d['info']
         
-    N_par = 0
-    txt_par = ""
+    N_par = np.shape(par)[0]
 
     #--------------------------------------
-    elif win_name == "Kaiser":
-        fn_name = "kaiser"
-        N_par = 1
-        txt_par = '&beta; ='
-        info = ("<span>Shape parameter; lower values reduce  main lobe width, "
-                   "higher values reduce side lobe level, typ. in the range 5 ... 20.</span>")
-        _get_param()
-        #if not self.param:
-         #   _set_param(5)
-    #--------------------------------------
-    elif win_name == "Chebwin":
-        fn_name = "chebwin"
-        N_par = 1
-        txt_par = 'Attn ='
-        info = ("<span>Side lobe attenuation in dB (typ. 80 dB).</span>")
-        _get_param()
-        if not param:
-            param = 80
-        if param < 45:
-            _set_param(45)
-            logger.warning("Attenuation needs to be larger than 45 dB!")
-    else:
-        logger.error("Unknown window type {0}".format(win_name))
 
     # get attribute fn_name from submodule sig.windows and
     # return the desired window function:
@@ -136,14 +113,12 @@ def calc_window_function(win_dict, win_name):
         logger.error("No window function {0} in scipy.signal.windows, using rectangular window instead!"\
                      .format(fn_name))
         win_fnct = sig.windows.boxcar
-        param = None
         
-    win_dict['win_name'] = win_name
-    win_dict['win_fnct'] = fn_name
+    win_dict.update({'win_name':win_name, 'win_fnct':fn_name, 'info':info})
+
     if N_par == 1:
-        win_dict['win_params'] = param
+        win_dict['win_params'] = par
     else:
         win_dict['win_params'] = ''
-    win_dict['win_len']  = N
     
     return win_fnct
