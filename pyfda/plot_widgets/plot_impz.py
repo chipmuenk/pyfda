9ff476d36e5e9ce78ffab84df578386aeb2ee770
Christian Muenker <mail@chipmuenk.de>
2020-01-31 22:04:10 +0100

    remove unneeded routines Signed-off-by: Christian Muenker <mail@chipmuenk.de>

--- pyfda/plot_widgets/plot_impz.py
+++ pyfda/plot_widgets/plot_impz.py
@@ -150,15 +150,6 @@ class Plot_Impz(QWidget):
         self.sig_rx.connect(self.process_sig_rx)
         # connect UI to widgets and signals upstream:
         self.ui.sig_tx.connect(self.process_sig_rx) 
-#------------------------------------------------------------------------------
-    def process_sig_rx_local(self, dict_sig=None):
-        """
-        Flag signals coming in from local subwidgets with `propagate=True` before
-        proceeding with processing in `process_sig_rx`.
-
-        TODO: not used at the moment
-        """
-        self.process_sig_rx(dict_sig, propagate=True)
 
 #------------------------------------------------------------------------------
     def process_sig_rx(self, dict_sig=None, propagate=False):
@@ -263,13 +254,6 @@ class Plot_Impz(QWidget):
 #                    self.needs_calc = True # always require recalculation when triggered externally
 #                    qstyle_widget(self.ui.but_run, "changed")
 #                    self.fx_select("Fixpoint")
-
-        if propagate:
-            # signals of local subwidgets are propagated, global signals terminate here.
-            # The next event in the queue is only handled when control returns
-            # from this one
-            self.sig_tx.emit(dict_sig)
-            return
 
 # =============================================================================
 # Simulation: Calculate stimulus, response and draw them
