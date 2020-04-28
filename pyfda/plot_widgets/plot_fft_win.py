Fix wrong calculation and display of Coherent Gain Signed-off-by: Christian Münker <mail@chipmuenk.de>

pyfda/plot_widgets/plot_fft_win.py
@@ -367,12 +367,12 @@ class Plot_FFT_win(QDialog):
 
 
         self.nenbw = self.N * np.sum(np.square(self.win)) / (np.square(np.sum(self.win)))
-        self.scale = self.N / np.sum(self.win)
+        self.cgain = np.sum(self.win) / self.N
 
         self.F = fftfreq(self.N * self.pad, d=1. / fb.fil[0]['f_S']) # use zero padding
         self.Win = np.abs(fft(self.win, self.N * self.pad))
         if self.chk_norm_f.isChecked():
-            self.Win /= (self.N / self.scale)# correct gain for periodic signals (coherent gain)
+            self.Win /= (self.N * self.cgain)# correct gain for periodic signals (coherent gain)
 
         first_zero = argrelextrema(self.Win[:(self.N*self.pad)//2], np.less)
         if np.shape(first_zero)[1] > 0:
@@ -426,14 +426,14 @@ class Plot_FFT_win(QDialog):
         if self.chk_log_f.isChecked():
             self.ax_f.plot(F, np.maximum(20 * np.log10(np.abs(Win)), self.bottom_f))
             self.nenbw_disp = 10 * np.log10(self.nenbw)
-            self.scale_disp = 20 * np.log10(self.scale)
+            self.cgain_disp = 20 * np.log10(self.cgain)
             self.sidelobe_level_disp = 20 * np.log10(self.sidelobe_level)
             self.unit_nenbw = "dB"
             self.unit_scale = "dB"
         else:
             self.ax_f.plot(F, Win)
             self.nenbw_disp = self.nenbw
-            self.scale_disp = self.scale
+            self.cgain_disp = self.cgain
             self.sidelobe_level_disp = self.sidelobe_level
             self.unit_nenbw = "bins"
             self.unit_scale = ""
@@ -473,7 +473,7 @@ class Plot_FFT_win(QDialog):
             labels_f.append("$NENBW$ = {0:.4g} {1}".format(self.nenbw_disp, self.unit_nenbw))
             N_patches += 1
         if self.tbl_sel[1]:
-            labels_f.append("$CGAIN$ = {0:.4g} {1}".format(self.scale_disp, self.unit_scale))
+            labels_f.append("$CGAIN$ = {0:.4g} {1}".format(self.cgain_disp, self.unit_scale))
             N_patches += 1
         if self.tbl_sel[2]:
             labels_f.append("1st Zero = {0:.4g}".format(self.first_zero_f))
@@ -499,7 +499,7 @@ class Plot_FFT_win(QDialog):
         self._set_table_item(0,1, "{0:.5g}".format(self.nenbw_disp))
         self._set_table_item(0,2, self.unit_nenbw)
         self._set_table_item(0,3, "Scale", font=self.bfont)#, sel=True)
-        self._set_table_item(0,4, "{0:.5g}".format(self.scale_disp))
+        self._set_table_item(0,4, "{0:.5g}".format(self.cgain_disp))
         self._set_table_item(0,5, self.unit_scale)
 
         self._set_table_item(1,0, "1st Zero", font=self.bfont)#, sel=True)

pyfda/plot_widgets/plot_impz.py
@@ -1062,7 +1062,7 @@ class Plot_Impz(QWidget):
                 unit_nenbw = "dB"
                 unit_cgain = "dB"
                 nenbw = 10 * np.log10(self.ui.nenbw)
-                cgain = 20 * np.log10(self.ui.scale)
+                cgain = 20 * np.log10(self.ui.cgain)
                 H_id = np.maximum(20 * np.log10(H_id), self.ui.bottom_f)
                 if plt_stimulus:
                     X = np.maximum(20 * np.log10(X), self.ui.bottom_f)
@@ -1081,7 +1081,7 @@ class Plot_Impz(QWidget):
                 unit_nenbw = "bins"
                 unit_cgain = ""
                 nenbw = self.ui.nenbw
-                cgain = self.ui.scale
+                cgain = self.ui.cgain
 
             XY_str = XY_str + ' in ' + unit
 

pyfda/plot_widgets/plot_impz_ui.py
@@ -811,8 +811,8 @@ class PlotImpz_UI(QWidget):
 
         self.nenbw = self.N * np.sum(np.square(self.win)) / (np.square(np.sum(self.win)))
 
-        self.scale = self.N / np.sum(self.win)
-        self.win *= self.scale # correct gain for periodic signals (coherent gain)
+        self.cgain = np.sum(self.win) / self.N # coherent gain
+        self.win /= self.cgain # correct gain for periodic signals
 
         # only emit a signal for local triggers to prevent infinite loop:
         # - signal-slot connection passes a bool or an integer