b2c2006b800ae30f18199d7323d03bedac90ce7f
Christian Münker <mail@chipmuenk.de>
2020-11-23 10:33:37 +0100

    implement simple allpass filter as a demo for filter widget design Signed-off-by: Christian Münker <mail@chipmuenk.de>

--- pyfda/widget_templates/filter_widgets/my_filter_widget.py
+++ pyfda/widget_templates/filter_widgets/my_filter_widget.py
@@ -30,20 +30,18 @@ from pyfda.libs.pyfda_lib import fil_save, safe_eval
 
 __version__ = "1.0"
 
-classes = {'Delay':'Delay'} #: Dict containing class name : display name
+classes = {'AllpPZ':'Allpass (P)'} #: Dict containing class name : display name
 
-class Delay(QWidget):
+class AllpPZ(QWidget):
 
     FRMT = 'zpk' # output format of delay filter widget
 
     info ="""
-**Delay widget**
+**Allpass widget**
 
-allows entering the number of **delays** :math:`N` :math:`T_S`. It is treated as a FIR filter,
-the number of delays is directly translated to a number of poles (:math:`N > 0`) 
-or zeros (:math:`N < 0`).
-
-Obviously, there is no minimum design algorithm or no design algorithm at all :-)
+allows entering the two **poles** :math:`p`. **zeros** are calculated from the 
+reciprocal values of the poles. There is no minimum algorithm, only the two 
+poles can be entered manually.
 
     """
 
@@ -52,16 +50,17 @@ Obviously, there is no minimum design algorithm or no design algorithm at all :-
     def __init__(self):
         QWidget.__init__(self)
 
-        self.N = 5
-
-        self.ft = 'FIR'
+        self.p = [0.5, 0.5j]
         
+        self.ft = 'IIR'
+        
+        # the following defines which subwidgets are "a"ctive, "i"nvisible or "d"eactivated
         self.rt_dicts = ('com',)
-
         self.rt_dict = {
-            'COM': {'man': {'fo':('a', 'N'),
+            'COM': {'man': {'fo':('d', 'N'),
                             'msg':('a', 
-                                "<span>Enter desired number of delays <b><i>N</i></b>.</span>")
+                                "<span>Enter poles  <b><i>p</i></b> for allpass function,"
+                                "zeros will be calculated.</span>")
                             },
                 },
             'AP': {'man':{}
@@ -77,18 +76,26 @@ Obviously, there is no minimum design algorithm or no design algorithm at all :-
         These subwidgets are instantiated dynamically when needed in 
         select_filter.py using the handle to the filter instance, fb.fil_inst.
         """
-        pass
-        self.lbl_delay = QLabel("Delays", self)
-        self.lbl_delay.setObjectName('wdg_lbl_delays')
-        self.led_delay = QLineEdit(self)
-        self.led_delay.setText(str(self.N))
-        self.led_delay.setObjectName('wdg_led_delay')
-        self.led_delay.setToolTip("Number of delays, N > 0 produces poles, N < 0 zeros.")
+        self.lbl_pole1 = QLabel("Pole 1", self)
+        self.lbl_pole1.setObjectName('wdg_lbl_pole1')
+        self.led_pole1 = QLineEdit(self)
+        self.led_pole1.setText(str(self.p[0]))
+        self.led_pole1.setObjectName('wdg_led_pole1')
+        self.led_pole1.setToolTip("Pole 1 for allpass filter")
+        
+        self.lbl_pole2 = QLabel("Pole 2", self)
+        self.lbl_pole2.setObjectName('wdg_lbl_pole2')
+        self.led_pole2 = QLineEdit(self)
+        self.led_pole2.setText(str(self.p[1]))
+        self.led_pole2.setObjectName('wdg_led_pole2')
+        self.led_pole2.setToolTip("Pole 2 for allpass filter")
 
         self.layHWin = QHBoxLayout()
         self.layHWin.setObjectName('wdg_layGWin')
-        self.layHWin.addWidget(self.lbl_delay)
-        self.layHWin.addWidget(self.led_delay)
+        self.layHWin.addWidget(self.lbl_pole1)
+        self.layHWin.addWidget(self.led_pole1)
+        self.layHWin.addWidget(self.lbl_pole2)
+        self.layHWin.addWidget(self.led_pole2)        
         self.layHWin.setContentsMargins(0,0,0,0)
         # Widget containing all subwidgets (cmbBoxes, Labels, lineEdits)
         self.wdg_fil = QWidget(self)
@@ -98,7 +105,8 @@ Obviously, there is no minimum design algorithm or no design algorithm at all :-
         #----------------------------------------------------------------------
         # SIGNALS & SLOTs
         #----------------------------------------------------------------------
-        self.led_delay.editingFinished.connect(self._update_UI)
+        self.led_pole1.editingFinished.connect(self._update_UI)
+        self.led_pole2.editingFinished.connect(self._update_UI)
         # fires when edited line looses focus or when RETURN is pressed
         #----------------------------------------------------------------------
 
@@ -111,18 +119,19 @@ Obviously, there is no minimum design algorithm or no design algorithm at all :-
         and converted to integer) and store parameter settings in filter 
         dictionary
         """
-        self.N = safe_eval(self.led_delay.text(), self.N, 
-                                      sign="poszero", return_type='int')
-        self.led_delay.setText(str(self.N))
+        self.p[0] = safe_eval(self.led_pole1.text(), self.p[0], return_type='cmplx')
+        self.led_pole1.setText(str(self.p[0]))
+
+        self.p[1] = safe_eval(self.led_pole2.text(), self.p[1], return_type='cmplx')
+        self.led_pole2.setText(str(self.p[1]))
 
         if not 'wdg_fil' in fb.fil[0]:
             fb.fil[0].update({'wdg_fil':{}})
-        fb.fil[0]['wdg_fil'].update({'delay':
-                                        {'N':self.N}
+        fb.fil[0]['wdg_fil'].update({'poles':{'p1':self.p[0], 'p2':self.p[1]}
                                     })
         
         # sig_tx -> select_filter -> filter_specs   
-        self.sig_tx.emit({'sender':__name__, 'filt_changed':'delay'})
+        self.sig_tx.emit({'sender':__name__, 'filt_changed':'pole_1_2'})
 
 
     def _load_dict(self):
@@ -131,28 +140,30 @@ Obviously, there is no minimum design algorithm or no design algorithm at all :-
         corresponding UI elements. _load_dict() is called upon initialization
         and when the filter is loaded from disk.
         """
-        if 'wdg_fil' in fb.fil[0] and 'delay' in fb.fil[0]['wdg_fil']:
-            wdg_fil_par = fb.fil[0]['wdg_fil']['delay']
-            if 'N' in wdg_fil_par:
-                self.N = wdg_fil_par['N']
-                self.led_delay.setText(str(self.N))
-
+        if 'wdg_fil' in fb.fil[0] and 'poles' in fb.fil[0]['wdg_fil']:
+            wdg_fil_par = fb.fil[0]['wdg_fil']['poles']
+            if 'p1' in wdg_fil_par:
+                self.p1 = wdg_fil_par['p1']
+                self.led_pole1.setText(str(self.p1))
+            if 'p2' in wdg_fil_par:
+                self.p2 = wdg_fil_par['p2']
+                self.led_pole2.setText(str(self.p2))
 
     def _get_params(self, fil_dict):
         """
-        Translate parameters from the passed dictionary to instance
-        parameters, scaling / transforming them if needed.
+        Get parameters needed for filter design from the passed dictionary and 
+        translate them to instance parameters, scaling / transforming them if needed.
         """
-        self.N     = fil_dict['N']  # filter order is translated to numb. of delays
-                                        
+        #self.p1     = fil_dict['zpk'][1][0]  # get the first and second pole
+        #self.p2     = fil_dict['zpk'][1][1]  # from central filter dect
+        logger.info(fil_dict['zpk'])
         
-    def _test_N(self):
+    def _test_poles(self):
         """
-        Warn the user if the calculated order is too high for a reasonable filter
-        design.
+        Warn the user if one of the poles is outside the unit circle
         """
-        if self.N > 2000:
-            return qfilter_warning(self, self.N, "Delay")
+        if abs(self.p[0]) >= 1 or abs(self.p[1])  >=1:
+            return qfilter_warning(self, self.p[0], "Delay")
         else:
             return True
 
@@ -163,15 +174,38 @@ Obviously, there is no minimum design algorithm or no design algorithm at all :-
         dictionary 'fil_dict'.
         """
         if arg is None:
-            arg = np.zeros(self.N)
-            #arg =[[0], np.zeros(self.N), 1] # crashes coeff tab
+            logger.error("Passed empty filter dict")
+        logger.info(arg)
         fil_save(fil_dict, arg, self.FRMT, __name__)
+        
+        fil_dict['N'] = len(self.p)
+
+
+#------------------------------------------------------------------------------
+# Filter design routines
+#------------------------------------------------------------------------------
+# The method name MUST be "FilterType"+"MinMan", e.g. LPmin or BPman
 
     def APman(self, fil_dict):
-        self._get_params(fil_dict)
-        if not self._test_N():
+        """
+        Calculate z =1/p* for a given set of poles p. If p=0, set z=0.
+        The gain factor k is calculated from z and p at z = 1.
+        """
+        self._get_params(fil_dict) # not needed here
+        if not self._test_poles():
             return -1
-        self._save(fil_dict)
+        self.z=[0,0]
+        if self.p[0] != 0:
+            self.z[0] = np.conj(1/self.p[0])
+        if type(self.p[0]) == complex:
+            pass
+        if self.p[1] != 0:
+            self.z[1] = np.conj(1/self.p[1])
+        
+        k = np.abs(np.polyval(np.poly(self.p),1) / np.polyval(np.poly(self.z),1))
+        zpk_list = [self.z,self.p,k]
+
+        self._save(fil_dict, zpk_list)
 
 #------------------------------------------------------------------------------
 
@@ -182,14 +216,14 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     
     # instantiate filter widget
-    filt = Delay()
+    filt = AllpPZ()
     filt.construct_UI()
-    wdg_delay = getattr(filt, 'wdg_fil')
+    wdg_allpass = getattr(filt, 'wdg_fil')
 
     layVDynWdg = QVBoxLayout()
-    layVDynWdg.addWidget(wdg_delay, stretch = 1)
+    layVDynWdg.addWidget(wdg_allpass, stretch = 1)
     
-    filt.LPman(fb.fil[0])  # design a low-pass with parameters from global dict
+    filt.APman(fb.fil[0])  # design an all pass filter with parameters from global dict
     print(fb.fil[0][filt.FRMT]) # return results in default format
 
     frmMain = QFrame()
