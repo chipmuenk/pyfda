1c1c72db977cf816dd111d74e5967007ba47c760
Christian Münker <mail@chipmuenk.de>
2021-02-06 14:40:15 +0100

    Add subclassed pushbutton with rich text

--- pyfda/libs/compat.py
+++ pyfda/libs/compat.py
@@ -85,6 +85,44 @@ class QFD(QFileDialog):
 
     def getSaveFileName_(self, **kwarg):
         return self.getSaveFileName(**kwarg)
+    
+class QPushButtonRT(QPushButton):
+    """
+    Subclass QPushButton to render rich text
+    """
+    def __init__(self, parent=None, text=None, margin=10):
+        if parent is not None:
+            super().__init__(parent)
+        else:
+            super().__init__()
+        self.__lbl = QLabel(self)
+        if text is not None:
+            self.__lbl.setText(text)
+        self.__lyt = QHBoxLayout()
+        self.__lyt.setContentsMargins(10, 0, 0, 0) # L, T, R, B
+        self.__lyt.setSpacing(0)
+        self.setLayout(self.__lyt)
+        self.__lbl.setAttribute(Qt.WA_TranslucentBackground)
+        self.__lbl.setAttribute(Qt.WA_TransparentForMouseEvents)
+        self.__lbl.setSizePolicy(
+            QSizePolicy.Expanding,
+            QSizePolicy.Expanding,
+        )
+        self.__lbl.setTextFormat(Qt.RichText)
+        self.__lyt.addWidget(self.__lbl)
+        return
+
+    def setText(self, text):
+        self.__lbl.setText(text)
+        self.updateGeometry()
+        return
+
+    def sizeHint(self):
+        s = QPushButton.sizeHint(self)
+        w = self.__lbl.sizeHint()
+        s.setWidth(w.width() + 20)
+        #s.setHeight(w.height())
+        return s
 
 
 if __name__ == '__main__':
