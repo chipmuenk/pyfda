40effa759962bbd693abbe230069e0c929ad5ace
Christian Muenker <mail@chipmuenk.de>
2021-03-03 17:37:49 +0100

    use qcmb_populate for combo box, autoformatter gone wild Signed-off-by: Christian Muenker <mail@chipmuenk.de>

--- pyfda/plot_widgets/plot_tau_g.py
+++ pyfda/plot_widgets/plot_tau_g.py
@@ -9,26 +9,27 @@
 """
 Widget for plotting the group delay
 """
+from matplotlib.ticker import AutoMinorLocator
+from pyfda.plot_widgets.mpl_widget import MplWidget
+from pyfda.pyfda_rc import params
+import pyfda.filterbroker as fb
+import numpy as np
+from pyfda.libs.pyfda_qt_lib import qset_cmb_box, qcmb_box_populate
+from pyfda.libs.pyfda_sig_lib import group_delay, group_delayz
+from pyfda.libs.compat import (Qt, QCheckBox, QWidget, QFrame, QComboBox,
+                               QHBoxLayout, pyqtSignal, pyqtSlot)
 import logging
 logger = logging.getLogger(__name__)
 
-from pyfda.libs.compat import (Qt, QCheckBox, QWidget, QFrame, QComboBox,
-                               QHBoxLayout, pyqtSignal, pyqtSlot)
-from pyfda.libs.pyfda_sig_lib import group_delay, group_delayz
-from pyfda.libs.pyfda_qt_lib import qset_cmb_box
 
-import numpy as np
-
-import pyfda.filterbroker as fb
-from pyfda.pyfda_rc import params
 #from scipy.signal import group_delay
-from pyfda.plot_widgets.mpl_widget import MplWidget
-from matplotlib.ticker import AutoMinorLocator
 
 # TODO: Anticausal filter have no group delay. But is a filter with
 #       'baA' always anticausal or maybe just acausal?
 
-classes = {'Plot_tau_g':'tau_g'} #: Dict containing class name : display name
+# : Dict containing class name : display name
+classes = {'Plot_tau_g': 'tau_g'}
+
 
 class Plot_tau_g(QWidget):
     """
@@ -38,14 +39,25 @@ class Plot_tau_g(QWidget):
     sig_rx = pyqtSignal(object)
 #    sig_tx = pyqtSignal(object) # outgoing from process_signals
 
-
     def __init__(self, parent):
         super(Plot_tau_g, self).__init__(parent)
-        self.verbose = False # suppress warnings
-        self.algorithm = "diff"
+        self.verbose = False  # suppress warnings
+        self.algorithm = "auto"
         self.needs_calc = True   # flag whether plot needs to be recalculated
         self.tool_tip = self.tr("Group delay")
-        self.tab_label = "\U0001D70F(f)"#"tau_g" \u03C4
+        self.tab_label = "\U0001D70F(f)"  # "tau_g" \u03C4
+
+        self.cmb_algorithm_items =\
+            ["<span>Select algorithm for calculating the group delay.</span>",
+             ("auto", "Auto", "<span>Try to find best-suited algorithm.</span>"),
+             ("scipy", "Scipy", "<span>Scipy algorithm.</span>"),
+             ("jos", "JOS", "<span>J.O. Smith's algorithm.</span>"),
+             ("shpak", "Shpak", "<span>Shpak's algorithm for SOS and other IIR"
+              "filters.</span>"),
+             ("diff", "Diff", "<span>Textbook-style, differentiate the phase."
+              "</span>")
+            ]
+
         self._construct_UI()
 
     def _construct_UI(self):
@@ -57,34 +69,17 @@ class Plot_tau_g(QWidget):
         self.chkWarnings = QCheckBox(self.tr("Verbose"), self)
         self.chkWarnings.setChecked(self.verbose)
         self.chkWarnings.setToolTip(self.tr("<span>Print messages about singular group delay"
-                                    "and calculation times."))
- 
-        self.cmbAlgorithm = QComboBox(self)
-        for t in [(self.tr("Auto"),"auto"),(self.tr("Scipy"),"scipy"), (self.tr("JOS"), "jos"),
-                  (self.tr("Diff"), "diff"), (self.tr("Shpak"), "shpak")]: # text, data
-            self.cmbAlgorithm.addItem(*t)
-        qset_cmb_box(self.cmbAlgorithm, self.algorithm,data=True)
-        self.cmbAlgorithm.setToolTip(self.tr("<span>Select algorithm for calculating "
-                                     "the group delay.</span>"))
-        self.cmbAlgorithm.setItemData(0, self.tr("<span>Try to find best-suited algorithm."
-                                      "</span>"),Qt.ToolTipRole)
-        self.cmbAlgorithm.setItemData(1, self.tr("<span>Scipy algorithm.</span>"),Qt.ToolTipRole)
-        self.cmbAlgorithm.setItemData(2, self.tr("<span>J.O. Smith's algorithm.</span>"),Qt.ToolTipRole)
-        self.cmbAlgorithm.setItemData(3, self.tr("<span>Textbook-style, differentiate "
-                                      "the phase.</span>"),Qt.ToolTipRole)
-        self.cmbAlgorithm.setItemData(4, self.tr("<span>Shpak's algorithm for SOS and other "
-                                      "IIR filters.</span>"),Qt.ToolTipRole)        
+                                            "and calculation times."))
 
-        #self.chkScipy = QCheckBox("Scipy", self)
-        #self.chkScipy.setChecked(False)
-        #self.chkScipy.setToolTip("Use scipy group delay routine")
- 
+        self.cmbAlgorithm = QComboBox(self)
+        qcmb_box_populate(self.cmbAlgorithm, self.cmb_algorithm_items, self.algorithm)
+
         layHControls = QHBoxLayout()
         layHControls.addStretch(10)
         layHControls.addWidget(self.chkWarnings)
-        #layHControls.addWidget(self.chkScipy)
+        # layHControls.addWidget(self.chkScipy)
         layHControls.addWidget(self.cmbAlgorithm)
- 
+
         # This widget encompasses all control subwidgets:
         self.frmControls = QFrame(self)
         self.frmControls.setObjectName("frmControls")
@@ -98,24 +93,24 @@ class Plot_tau_g(QWidget):
         self.setLayout(self.mplwidget.layVMainMpl)
 
         self.init_axes()
-        self.draw() # initial drawing of tau_g
+        self.draw()  # initial drawing of tau_g
 
-        #----------------------------------------------------------------------
+        # ----------------------------------------------------------------------
         # GLOBAL SIGNALS & SLOTs
-        #----------------------------------------------------------------------
+        # ----------------------------------------------------------------------
         self.sig_rx.connect(self.process_sig_rx)
-        #----------------------------------------------------------------------
+        # ----------------------------------------------------------------------
         # LOCAL SIGNALS & SLOTs
-        #----------------------------------------------------------------------
+        # ----------------------------------------------------------------------
         self.mplwidget.mplToolbar.sig_tx.connect(self.process_sig_rx)
         self.cmbAlgorithm.currentIndexChanged.connect(self.draw)
 
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
     def process_sig_rx(self, dict_sig=None):
         """
         Process signals coming from the navigation toolbar and from sig_rx
         """
-        logger.debug("Processing {0} | needs_calc = {1}, visible = {2}"\
+        logger.debug("Processing {0} | needs_calc = {1}, visible = {2}"
                      .format(dict_sig, self.needs_calc, self.isVisible()))
         if self.isVisible():
             if 'data_changed' in dict_sig or 'home' in dict_sig or self.needs_calc:
@@ -127,17 +122,17 @@ class Plot_tau_g(QWidget):
             if 'data_changed' in dict_sig or 'view_changed' in dict_sig:
                 self.needs_calc = True
 
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
     def init_axes(self):
         """
         Initialize the axes and set some stuff that is not cleared by
         `ax.clear()` later on.
         """
         self.ax = self.mplwidget.fig.subplots()
-        self.ax.xaxis.tick_bottom() # remove axis ticks on top
-        self.ax.yaxis.tick_left() # remove axis ticks right
+        self.ax.xaxis.tick_bottom()  # remove axis ticks on top
+        self.ax.yaxis.tick_left()  # remove axis ticks right
 
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
     def calc_tau_g(self):
         """
         (Re-)Calculate the complex frequency response H(f)
@@ -148,31 +143,31 @@ class Plot_tau_g(QWidget):
         # calculate H_cmplx(W) (complex) for W = 0 ... 2 pi:
         # scipy: self.W, self.tau_g = group_delay((bb, aa), w=params['N_FFT'], whole = True)
 
-        if fb.fil[0]['creator'][0] == 'sos': # one of 'sos', 'zpk', 'ba'
+        if fb.fil[0]['creator'][0] == 'sos':  # one of 'sos', 'zpk', 'ba'
             self.W, self.tau_g = group_delay(fb.fil[0]['sos'], nfft=params['N_FFT'], sos=True,
-                                         whole=True, verbose=self.chkWarnings.isChecked(),
-                                         alg=self.cmbAlgorithm.currentData())
+                                             whole=True, verbose=self.chkWarnings.isChecked(),
+                                             alg=self.cmbAlgorithm.currentData())
         else:
             self.W, self.tau_g = group_delay(bb, aa, nfft=params['N_FFT'], whole=True,
-                                         verbose=self.chkWarnings.isChecked(),
-                                         alg=self.cmbAlgorithm.currentData()) # self.chkWarnings.isChecked())
+                                             verbose=self.chkWarnings.isChecked(),
+                                             alg=self.cmbAlgorithm.currentData())  # self.chkWarnings.isChecked())
 
         # Zero phase filters have no group delay (Causal+AntiCausal)
         if 'baA' in fb.fil[0]:
-           self.tau_g = np.zeros(self.tau_g.size)
+            self.tau_g = np.zeros(self.tau_g.size)
 
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
     def draw(self):
         self.calc_tau_g()
         self.update_view()
 
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
     def update_view(self):
         """
         Draw the figure with new limits, scale etc without recalculating H(f)
         """
-        #========= select frequency range to be displayed =====================
-        #=== shift, scale and select: W -> F, H_cplx -> H_c
+        # ========= select frequency range to be displayed =====================
+        # === shift, scale and select: W -> F, H_cplx -> H_c
         f_max_2 = fb.fil[0]['f_max'] / 2.
         F = self.W * f_max_2 / np.pi
 
@@ -184,12 +179,12 @@ class Plot_tau_g(QWidget):
             # only use the first half of H and F
             tau_g = self.tau_g[0:params['N_FFT']//2]
             F = F[0:params['N_FFT']//2]
-        else: # fb.fil[0]['freqSpecsRangeType'] == 'whole'
+        else:  # fb.fil[0]['freqSpecsRangeType'] == 'whole'
             # use H and F as calculated
             tau_g = self.tau_g
 
-        #================ Main Plotting Routine =========================
-        #===  clear the axes and (re)draw the plot
+        # ================ Main Plotting Routine =========================
+        # ===  clear the axes and (re)draw the plot
 
         if fb.fil[0]['freq_specs_unit'] in {'f_S', 'f_Ny'}:
             tau_str = r'$ \tau_g(\mathrm{e}^{\mathrm{j} \Omega}) / T_S \; \rightarrow $'
@@ -198,30 +193,34 @@ class Plot_tau_g(QWidget):
                 + ' in ' + fb.fil[0]['plt_tUnit'] + r' $ \rightarrow $'
             tau_g = tau_g / fb.fil[0]['f_S']
 
-        #---------------------------------------------------------
-        self.ax.clear() # need to clear, doesn't overwrite
-        line_tau_g, = self.ax.plot(F, tau_g, label = "Group Delay")
-        #---------------------------------------------------------
+        # ---------------------------------------------------------
+        self.ax.clear()  # need to clear, doesn't overwrite
+        line_tau_g, = self.ax.plot(F, tau_g, label="Group Delay")
+        # ---------------------------------------------------------
 
-        self.ax.xaxis.set_minor_locator(AutoMinorLocator()) # enable minor ticks
-        self.ax.yaxis.set_minor_locator(AutoMinorLocator()) # enable minor ticks
+        self.ax.xaxis.set_minor_locator(
+            AutoMinorLocator())  # enable minor ticks
+        self.ax.yaxis.set_minor_locator(
+            AutoMinorLocator())  # enable minor ticks
         self.ax.set_title(r'Group Delay $ \tau_g$')
         self.ax.set_xlabel(fb.fil[0]['plt_fLabel'])
         self.ax.set_ylabel(tau_str)
         # widen y-limits to suppress numerical inaccuracies when tau_g = constant
-        self.ax.set_ylim([max(np.nanmin(tau_g)-0.5,0), np.nanmax(tau_g) + 0.5])
+        self.ax.set_ylim(
+            [max(np.nanmin(tau_g)-0.5, 0), np.nanmax(tau_g) + 0.5])
         self.ax.set_xlim(fb.fil[0]['freqSpecsRange'])
 
         self.redraw()
 
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
     def redraw(self):
         """
         Redraw the canvas when e.g. the canvas size has changed
         """
         self.mplwidget.redraw()
 
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+
 
 def main():
     import sys
@@ -231,6 +230,7 @@ def main():
     app.setActiveWindow(mainw)
     mainw.show()
     sys.exit(app.exec_())
+
 
 if __name__ == "__main__":
     main()
