d6a3bba953ff13169e722d9de3eaaa9e0cb38f2f
Christian Münker <mail@chipmuenk.de>
2022-02-23 11:03:07 +0100

    Move separator to GridLayout

--- pyfda/plot_widgets/tran/plot_tran_stim_ui.py
+++ pyfda/plot_widgets/tran/plot_tran_stim_ui.py
@@ -144,7 +144,7 @@ class Plot_Tran_Stim_UI(QWidget):
         # combobox tooltip + data / text / tooltip for file I/O usage
         self.cmb_file_io_items = [
             ("<span>Select data from File I/O widget/span>"),
-            ("none", "None", "<span>Don't use file I/O data.</span>"),
+            ("off", "Off", "<span>Don't use file I/O data.</span>"),
             ("use", "Use", "<span><b>Use></b> file I/O data as stimuli.</span>"),
             ("add", "Add", "<span><b>Add</b> file I/O data to other stimuli")
             ]
@@ -230,17 +230,15 @@ class Plot_Tran_Stim_UI(QWidget):
         self.cmb_file_io = QComboBox(self)
         qcmb_box_populate(
             self.cmb_file_io, self.cmb_file_io_items, self.cmb_file_io_default)
-        line1 = QVLine()
-        line2 = QVLine(width=5)
-        layH_stim_io = QHBoxLayout()
-        layH_stim_io.setContentsMargins(0, 0, 0, 0)
+        # layH_stim_io = QHBoxLayout()
+        # layH_stim_io.setContentsMargins(0, 0, 0, 0)
         layV_stim_io = QVBoxLayout()
         layV_stim_io.setContentsMargins(0, 0, 0, 0)
         layV_stim_io.addWidget(self.lbl_file_io)
         layV_stim_io.addWidget(self.cmb_file_io)
-        layH_stim_io.addLayout(layV_stim_io)
-        layH_stim_io.addWidget(line1)
+        # layH_stim_io.addLayout(layV_stim_io)
         # -------------------------------------
+        line1 = QVLine(width=5)  # width = 5
         self.cmbStimulus = QComboBox(self)
         qcmb_box_populate(self.cmbStimulus, self.cmb_stim_items, self.cmb_stim_item)
 
@@ -411,7 +409,9 @@ class Plot_Tran_Stim_UI(QWidget):
 
         self.layGStim = QGridLayout()
         self.layGStim.setContentsMargins(0, 0, 0, 0)
-        i = 1
+        i = 0
+        self.layGStim.addWidget(line1, 0, i, 2, 1)  # fromRow, fromCol, rowSpan, colSpan
+        i += 1
         self.layGStim.addLayout(layHCmbStim, 0, i)
         self.layGStim.addLayout(layHStimDC, 1, i)
         i += 1
@@ -489,7 +489,7 @@ class Plot_Tran_Stim_UI(QWidget):
         # Main Widget
         # ----------------------------------------------------------------------
         layH_stim_par = QHBoxLayout()
-        layH_stim_par.addLayout(layH_stim_io)
+        layH_stim_par.addLayout(layV_stim_io)
         layH_stim_par.addWidget(self.frmGStim)
 
         layV_stim = QVBoxLayout()
