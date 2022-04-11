68e7dc8ca6de6f9f074c0f52d833b4b921971899
Christian Münker <mail@chipmuenk.de>
2022-04-11 10:02:39 +0200

    fix label Signed-off-by: Christian Münker <mail@chipmuenk.de>

--- pyfda/fixpoint_widgets/fir_df/fir_df_pyfixp_ui.py
+++ pyfda/fixpoint_widgets/fir_df/fir_df_pyfixp_ui.py
@@ -63,7 +63,7 @@ class FIR_DF_pyfixp_UI(QWidget):
             logger.warning("QCB key missing")
         self.wdg_wq_coeffs = FX_UI_WQ(
             fb.fil[0]['fxqc']['QCB'], wdg_name='wq_coeffs',
-            label='<b>Coeff. Quantization <i>B<sub>I.F&nbsp;</sub></i>:</b>')
+            label='<b>Coeff. Quantization <i>b<sub>I.F&nbsp;</sub></i>:</b>')
         layV_wq_coeffs = QVBoxLayout()
         layV_wq_coeffs.addWidget(self.wdg_wq_coeffs)
         self.frm_wq_coeffs = QFrame(self)
