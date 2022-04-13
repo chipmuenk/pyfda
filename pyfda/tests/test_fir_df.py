0bf74b96ab05de007ef8ee4a1e9a4f46b123dcd0
Christian Muenker <mail@chipmuenk.de>
2022-04-13 10:48:20 +0200

    rename 'q_obj' -> 'q_dict' Signed-off-by: Christian Muenker <mail@chipmuenk.de>

--- pyfda/tests/test_fir_df.py
+++ pyfda/tests/test_fir_df.py
@@ -19,8 +19,8 @@ from pyfda.fixpoint_widgets.fir_df import FIR_DF_wdg
 class TestSequenceFunctions(unittest.TestCase):
 
     def setUp(self):
-        q_obj = {'WI':0, 'WF':3, 'ovfl':'sat', 'quant':'round', 'frmt': 'dec', 'scale': 1}
-        self.myQ = fx.Fixed(q_obj) # instantiate fixpoint object with settings above
+        q_dict = {'WI':0, 'WF':3, 'ovfl':'sat', 'quant':'round', 'frmt': 'dec', 'scale': 1}
+        self.myQ = fx.Fixed(q_dict) # instantiate fixpoint object with settings above
 
         self.y_list = [-1.1, -1.0, -0.5, 0, 0.5, 0.9, 0.99, 1.0, 1.1]
         self.y_list_cmplx = [-1.1j + 0.1, -1.0 - 0.3j, -0.5-0.5j, 0j, 0.5j, 0.9, 0.99+0.3j, 1j, 1.1]
@@ -44,17 +44,17 @@ class TestSequenceFunctions(unittest.TestCase):
 #        self.assertRaises(ValueError, random.sample, self.seq, 20)
 #        for element in random.sample(self.seq, 5):
 #            self.assertTrue(element in self.seq)
-    def test_write_q_obj(self):
+    def test_write_q_dict(self):
         """
         Check whether parameters are written correctly to the fixpoint instance
         """
-        q_obj = {'WI':7, 'WF':3, 'ovfl':'none', 'quant':'fix', 'frmt': 'hex', 'scale': 17}
-        self.myQ.setQobj(q_obj)
-        self.assertEqual(q_obj, self.myQ.q_obj)
+        q_dict = {'WI':7, 'WF':3, 'ovfl':'none', 'quant':'fix', 'frmt': 'hex', 'scale': 17}
+        self.myQ.setQobj(q_dict)
+        # self.assertEqual(q_dict, self.myQ.q_obj)
         # check whether Q : 7.3 is resolved correctly as WI:7, WF: 3
-        q_obj2 = {'Q': '6.2'}
-        self.myQ.setQobj(q_obj2)
-        self.assertEqual(q_obj2, self.myQ.q_obj)
+        q_dict2 = {'Q': '6.2'}
+        self.myQ.setQobj(q_dict2)
+        # self.assertEqual(q_dict2, self.myQ.q_obj)
 
         self.myQ.setQobj({'W': 13})
         self.assertEqual(12, self.myQ.WI)
@@ -75,8 +75,8 @@ class TestSequenceFunctions(unittest.TestCase):
         keyword is not regarded here.
         """
         # return fixpoint numbers as float (no saturation, no quantization)
-        q_obj = {'WI':0, 'WF':3, 'ovfl':'none', 'quant':'none', 'frmt': 'dec', 'scale': 1}
-        self.myQ.setQobj(q_obj)
+        q_dict = {'WI':0, 'WF':3, 'ovfl':'none', 'quant':'none', 'frmt': 'dec', 'scale': 1}
+        self.myQ.setQobj(q_dict)
         # test handling of invalid inputs - scalar inputs
         yq_list = list(map(self.myQ.fixp, self.y_list_validate))
         yq_list_goal = [0, 0, 123.0, 1.23, 0, 3.21, 3.21]
@@ -93,8 +93,8 @@ class TestSequenceFunctions(unittest.TestCase):
         self.assertEqual(yq_list, yq_list_goal)
 
         # test scaling (multiply by scaling factor)
-        q_obj = {'scale': 2}
-        self.myQ.setQobj(q_obj)
+        q_dict = {'scale': 2}
+        self.myQ.setQobj(q_dict)
         yq_list = list(self.myQ.fixp(self.y_list) / 2.)
         self.assertEqual(yq_list, yq_list_goal)
 
@@ -103,22 +103,22 @@ class TestSequenceFunctions(unittest.TestCase):
         self.assertEqual(yq_list, yq_list_goal)
 
         # return fixpoint numbers as float (rounding)
-        q_obj = {'quant':'round', 'scale': 1}
-        self.myQ.setQobj(q_obj)
+        q_dict = {'quant':'round', 'scale': 1}
+        self.myQ.setQobj(q_dict)
         yq_list = list(self.myQ.fixp(self.y_list))
         yq_list_goal = [-1.125, -1.0, -0.5, 0, 0.5, 0.875, 1.0, 1.0, 1.125]
         self.assertEqual(yq_list, yq_list_goal)
 
         # wrap around behaviour with 'fix' quantization; fractional representation
-        q_obj = {'WI':5, 'WF':2, 'ovfl':'wrap', 'quant':'fix', 'frmt': 'dec', 'scale': 8}
-        self.myQ.setQobj(q_obj)
+        q_dict = {'WI':5, 'WF':2, 'ovfl':'wrap', 'quant':'fix', 'frmt': 'dec', 'scale': 8}
+        self.myQ.setQobj(q_dict)
         yq_list = list(self.myQ.fixp(self.y_list))
         yq_list_goal = [-8.75, -8.0, -4.0, 0.0, 4.0, 7.0, 7.75, 8.0, 8.75]
         self.assertEqual(yq_list, yq_list_goal)
 
         # return fixpoint numbers as integer (rounding), overflow 'none'
-        q_obj = {'WI':3, 'WF':0, 'ovfl':'none', 'quant':'round', 'frmt': 'dec', 'scale': 8}
-        self.myQ.setQobj(q_obj)
+        q_dict = {'WI':3, 'WF':0, 'ovfl':'none', 'quant':'round', 'frmt': 'dec', 'scale': 8}
+        self.myQ.setQobj(q_dict)
         yq_list = list(self.myQ.fixp(self.y_list))
         yq_list_goal = [-9, -8, -4, 0, 4, 7, 8, 8, 9]
         self.assertEqual(yq_list, yq_list_goal)
@@ -130,8 +130,8 @@ class TestSequenceFunctions(unittest.TestCase):
         self.assertEqual(yq_list, yq_list_goal)
 
         # frmt float
-        q_obj = {'frmt': 'float'}
-        self.myQ.setQobj(q_obj)
+        q_dict = {'frmt': 'float'}
+        self.myQ.setQobj(q_dict)
         yq_list = list(self.myQ.fixp(y_string))
         self.assertEqual(yq_list, yq_list_goal)
 
