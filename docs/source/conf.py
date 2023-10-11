aec95fedb35164886375569c088ff9022bc3211f
Christian Muenker <mail@chipmuenk.de>
2023-10-11 18:00:04 +0200

    Fix two more configuration problems Signed-off-by: Christian Muenker <mail@chipmuenk.de>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -89,7 +89,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -114,7 +114,7 @@ rst_prolog = """
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme' #alabaster'
+html_theme = 'classic' #alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
