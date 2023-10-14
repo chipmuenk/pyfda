2cb5397ad8f44781a5622cee238fc92018191710
Christian Muenker <mail@chipmuenk.de>
2023-10-14 19:49:11 +0200

    Add functions for comparing dictionaries recursively Signed-off-by: Christian Muenker <mail@chipmuenk.de>

--- pyfda/filterbroker.py
+++ pyfda/filterbroker.py
@@ -359,3 +359,79 @@ def redo():
 
 # Define dictionary with default settings for  FiXpoint Quantization and Coefficients:
 # 'fxqc'
+
+# Comparing nested dicts
+# https://stackoverflow.com/questions/27265939/comparing-python-dictionaries-and-nested-dictionaries
+
+d1= {'as': 1, 'a':
+        {'b':
+            {'cs':10,
+             'qqq': {'qwe':1}
+            },
+            'd': {'csd':30}
+        }
+    }
+d2= {'as': 3, 'a':
+        {'b':
+            {'cs':30,
+             'qqq': 123
+            },
+            'd':{'csd':20}
+        },
+        'newa':
+        {'q':
+            {'cs':50}
+        }
+    }
+def compare_dictionaries(dict_1, dict_2, dict_1_name, dict_2_name, path=""):
+    """Compare two dictionaries recursively to find non matching elements
+
+    Args:
+        dict_1: dictionary 1
+        dict_2: dictionary 2
+
+    Returns: string
+
+    """
+    err = ''
+    key_err = ''
+    value_err = ''
+    old_path = path
+    for k in dict_1.keys():
+        path = old_path + "[%s]" % k
+        if not dict_2.has_key(k):
+            key_err += "Key %s%s not in %s\n" % (dict_1_name, path, dict_2_name)
+        else:
+            if isinstance(dict_1[k], dict) and isinstance(dict_2[k], dict):
+                err += compare_dictionaries(dict_1[k],dict_2[k],'d1','d2', path)
+            else:
+                if dict_1[k] != dict_2[k]:
+                    value_err += "Value of %s%s (%s) not same as %s%s (%s)\n"\
+                        % (dict_1_name, path, dict_1[k], dict_2_name, path, dict_2[k])
+
+    for k in dict_2.keys():
+        path = old_path + "[%s]" % k
+        if not dict_1.has_key(k):
+            key_err += "Key %s%s not in %s\n" % (dict_2_name, path, dict_1_name)
+
+    return key_err + value_err + err
+
+# simpler version
+d1= {'a':{'b':{'cs':10},'d':{'cs':20}}}
+d2= {'a':{'b':{'cs':30} ,'d':{'cs':20}},'newa':{'q':{'cs':50}}}
+
+def findDiff(d1, d2, path=""):
+    for k in d1:
+        if k in d2:
+            if type(d1[k]) is dict:
+                findDiff(d1[k],d2[k], "%s -> %s" % (path, k) if path else k)
+            if d1[k] != d2[k]:
+                result = [ "%s: " % path, " - %s : %s" % (k, d1[k]) , " + %s : %s" % (k, d2[k])]
+                print("\n".join(result))
+        else:
+            print ("%s%s as key not in d2\n" % ("%s: " % path if path else "", k))
+
+print("comparing d1 to d2:")
+findDiff(d1,d2)
+print("comparing d2 to d1:")
+findDiff(d2,d1)
