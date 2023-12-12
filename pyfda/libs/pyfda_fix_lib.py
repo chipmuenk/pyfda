406882d4f5e1207ee2d88381dae1646f2df90b89
Christian Münker <mail@chipmuenk.de>
2023-12-12 09:44:29 +0100

    Start implementing requant() method equivalent to the amaranth method Signed-off-by: Christian Münker <mail@chipmuenk.de>

--- pyfda/libs/pyfda_fix_lib.py
+++ pyfda/libs/pyfda_fix_lib.py
@@ -806,6 +806,97 @@ class Fixed(object):
         self.N_over = 0
         self.N = 0
 
+    # --------------------------------------------------------------------------
+    def requant(self, x, QI, QO):
+
+        """
+        Change word length of input signal `sig_i` to the wordlength of the output
+        signal, using the quantization and saturation methods specified by
+        ``QO['quant']`` and ``QO['ovfl']``.
+
+        Parameters
+        ----------
+        x: input data, scalar or array-like
+            Signal to be requantized
+
+        QI: dict
+            Quantization dict for input word, only the keys 'WI' and 'WF' for integer
+            and fractional wordlength are evaluated. QI['WI'] = 2 and QI['WF'] = 13
+            e.g. define Q-Format '2.13'  with 2 integer, 13 fractional bits and 1 implied
+            sign bit = 16 bits total.
+
+        QO: dict
+            Quantization dict for output word format; the keys 'WI' and 'WF' for
+            integer and fractional wordlength are evaluated as well as the keys 'quant'
+            and 'ovfl' describing requantization and overflow behaviour.
+
+        Returns
+        -------
+
+        y: output data with same shape as input data
+            Requantized signal
+
+        Documentation
+        -------------
+
+        **Input and output word are aligned at their binary points.**
+
+        The following shows an example of rescaling an input word from Q2.4 to Q0.3
+        using wrap-around and truncation. It's easy to see that for simple wrap-around
+        logic, the sign of the result may change.
+
+        ::
+
+        S | WI1 | WI0 * WF0 | WF1 | WF2 | WF3  :  WI = 2, WF = 4, W = 7
+        0 |  1  |  0  *  1  |  0  |  1  |  1   =  43 (dec) or 43/16 = 2 + 11/16 (float)
+                      *
+                |  S  * WF0 | WF1 | WF2        :  WI = 0, WF = 3, W = 4
+                   0  *  1  |  0  |  1         =  7 (dec) or 7/8 (float)
+
+
+        The float or "real (world) value" is calculated by multiplying the integer
+        value by 2 ** (-WF).
+
+        For requantizing two numbers to the same WI and WF, imagine both binary numbers
+        to be right-aligned. Changes in the number of integer bits `dWI` and fractional
+        bits `dWF` are handled separately.
+
+        Fractional Bits
+        ---------------
+
+        - For reducing the number of fractional bits by `dWF`, simply right-shift the
+          integer number by `dWF`. For rounding, add '1' to the bit below the truncation
+          point before right-shifting.
+
+        - Extend the number of fractional bits by left-shifting the integer by `dWF`,
+          LSB's are filled with zeros.
+
+        Integer Bits
+        ------------
+
+        - For reducing the number of integer bits by `dWI`, simply right-shift the
+          integer by `dWI`.
+
+        - The number of fractional bits is SIGN-EXTENDED by filling up the left-most
+          bits with the sign bit.
+
+        """
+        WI_I = QI['WI']         # number of integer bits (input signal)
+        WI_F = QI['WF']         # number of fractional bits (input signal)
+        WI   = WI_I + WI_F + 1  # total word length (input signal)
+
+        WO_I = QO['WI']         # number of integer bits (output signal)
+        WO_F = QO['WF']         # number of fractional bits (output signal)
+        WO   = WO_I + WO_F + 1  # total word length (output signal)
+
+        dWF = WI_F - WO_F       # difference of fractional lengths
+        dWI = WI_I - WO_I       # difference of integer lengths
+
+        # max. resp. min, output values
+        MIN_o = - 1 << (WO - 1)
+        MAX_o = -MIN_o - 1
+
+
 
     # --------------------------------------------------------------------------
     def frmt2float(self, y):
