6268bf7bcf7c6dd1f298cd4e0b30ad5957715922
Christian Muenker <mail@chipmuenk.de>
2024-04-07 22:08:06 +0200

    remove myst_parser Signed-off-by: Christian Muenker <mail@chipmuenk.de>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -49,7 +49,6 @@ release = version
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'myst_parser',
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
     'sphinx.ext.mathjax',
