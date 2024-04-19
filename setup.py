e2538c33d4256b84a4a91bd8c01ed4a4e61daede
Christian Münker <mail@chipmuenk.de>
2024-04-19 11:07:59 +0200

    Update python version requirements Signed-off-by: Christian Münker <mail@chipmuenk.de>

--- setup.py
+++ setup.py
@@ -69,11 +69,11 @@ setup(
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
     ],
     # automatically find top-level package and sub-packages input_widgets,
     # plot_widgets etc.:
