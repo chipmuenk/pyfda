2d7630675098b77bc4e4062ed509b3d084eb8708
Christian Münker <mail@chipmuenk.de>
2024-04-23 21:08:15 +0200

    make overflow counter of accumulator visible again Signed-off-by: Christian Münker <mail@chipmuenk.de>

--- pyfda/fixpoint_widgets/fir_df/fir_df_amaranth_ui.py
+++ pyfda/fixpoint_widgets/fir_df/fir_df_amaranth_ui.py
@@ -104,7 +104,7 @@ class FIR_DF_amaranth_UI(QWidget):
         self.wdg_wq_accu = FX_UI_WQ(
             fb.fil[0]['fxq']['QACC'], objectName='fx_ui_wq_fir_df_accu',
             cmb_w_vis='on', cmb_w_items=self.cmb_wq_accu_items,
-            count_ovfl_vis='off',
+            count_ovfl_vis='auto',
             label='<b>Accu Format <i>Q<sub>ACC&nbsp;</sub></i>:</b>',
             cmb_ov_items=["<span>Select overflow behaviour.</span>",
                   ("wrap", "Wrap", "Two's complement wrap around"),
