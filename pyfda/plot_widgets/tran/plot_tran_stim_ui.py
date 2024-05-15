9529ebc5ddce5923350631a2f83bf467bf826825
Christian Muenker <mail@chipmuenk.de>
2024-05-15 17:38:23 +0200

    Frequency range checking is now performed directly in functions Signed-off-by: Christian Muenker <mail@chipmuenk.de>

--- pyfda/plot_widgets/tran/plot_tran_stim_ui.py
+++ pyfda/plot_widgets/tran/plot_tran_stim_ui.py
@@ -683,12 +683,8 @@ class Plot_Tran_Stim_UI(QWidget):
                     scale = getattr(self, param_name)  # get scale value
                     var_old = getattr(self, var_name)  # get old var value
                     # assign var with either content of text field or fallback value:
-                    if var_name in {'T1', 'T2'}:
-                        var = safe_eval(source.text(), var_old * scale,
-                                    return_type='float') / scale
-                    else:
-                        var = safe_eval(source.text(), var_old * scale,
-                                        sign='pos', return_type='float') / scale
+                    var = safe_eval(
+                        source.text(), var_old * scale, return_type='float') / scale
                     # assign evaluated text field to variable
                     setattr(self, var_name, var)
                     # set textfield with scaled value of `var_name`:
