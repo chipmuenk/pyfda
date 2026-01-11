1d2e2410da5313d2dc45bfff2ce7103eba3c0937
Christian Münker <mail@chipmuenk.de>
2026-01-11 16:30:25 +0100

    create separate file for widget_class_dicts Signed-off-by: Christian Münker <mail@chipmuenk.de>

---
+++ pyfda/libs/widget_class_dicts.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of the pyFDA project hosted at https://github.com/chipmuenk/pyfda
+#
+# Copyright © pyFDA Project Contributors
+# Licensed under the terms of the MIT License
+# (see file LICENSE in root directory for details)
+
+# Dynamic parameters and settings are exchanged via the dictionaries in this file.
+# Importing ``filterbroker.py`` runs the module once, defining all module variables
+# which have a global scope like class variables.
+
+# -----------------------------------------------------------------------------
+# This file contains dicts with widget class names from the main configuration file, parsed in 
+# `tree_builder._build_widget_class_dicts()`. Those initial definitions are only meant as examples, 
+# for documentation and for module test, they are overwritten during the initialization.
+#
+# The keys are the class names of the widgets that have been parsed in the configuration file
+# and successfully instantiated. The values are dicts containing display names (e.g. for tabs
+# and comboboxes) and module paths in dotted notation for dynamic importing.
+#------------------------------------------------------------------------------
+
+from collections import OrderedDict
+
+plot_classes = OrderedDict(
+    [('Plot_Hf', {'name': '|H(f)|', 'mod': 'pyfda.plot_widgets.plot_hf'}),
+     ('Plot_Phi', {'name': 'φ(f)', 'mod': 'pyfda.plot_widgets.plot_phi'}),
+     ('Plot_tau_g', {'name': 'tau_g', 'mod': 'pyfda.plot_widgets.plot_tau_g'}),
+     ('Plot_PZ', {'name': 'P / Z', 'mod': 'pyfda.plot_widgets.plot_pz'}),
+     ('Plot_Impz', {'name': 'h[n]', 'mod': 'pyfda.plot_widgets.plot_impz'}),
+     ('Plot_3D', {'name': '3D', 'mod': 'pyfda.plot_widgets.plot_3d'})
+     ])
+input_classes = OrderedDict(
+    [('Input_Specs', {'name': 'Specs', 'mod': 'pyfda.input_widgets.input_specs'}),
+     ('Input_Coeffs', {'name': 'b,a', 'mod': 'pyfda.input_widgets.input_coeffs'}),
+     ('Input_PZ', {'name': 'P/Z', 'mod': 'pyfda.input_widgets.input_pz'}),
+     ('Input_Info', {'name': 'Info', 'mod': 'pyfda.input_widgets.input_info'}),
+     ('Input_Files', {'name': 'Files', 'mod': 'pyfda.input_widgets.input_files'}),
+     ('Input_Fixpoint_Specs', {'name': 'Fixpoint',
+                               'mod': 'pyfda.input_widgets.input_fixpoint_specs'})
+     ])
+
+fixpoint_classes = OrderedDict(
+    [('FIR_DF_wdg', {'name': 'FIR_DF',
+                     'mod': 'pyfda.fixpoint_widgets.fir_df', 'opt': ['Equiripple', 'Firwin']}),
+     ('Delay_wdg', {'name': 'Delay',
+                    'mod': 'pyfda.fixpoint_widgets.delay1', 'opt': ['Equiripple']})
+     ])
+
+filter_classes = OrderedDict(
+    [# IIR
+     ('Bessel', {'name': 'Bessel', 'mod': 'pyfda.filter_widgets.bessel'}),
+     ('Butter', {'name': 'Butterworth', 'mod': 'pyfda.filter_widgets.butter'}),
+     ('Cheby1', {'name': 'Chebyshev 1', 'mod': 'pyfda.filter_widgets.cheby1'}),
+     ('Cheby2', {'name': 'Chebyshev 2', 'mod': 'pyfda.filter_widgets.cheby2'}),
+     ('Ellip', {'name': 'Elliptic', 'mod': 'pyfda.filter_widgets.ellip'}),
+     ('EllipZeroPhz', {'name': 'EllipZeroPhz', 'mod': 'pyfda.filter_widgets.ellip_zero'}),
+     # FIR
+     ('Equiripple', {'name': 'Equiripple', 'mod': 'pyfda.filter_widgets.equiripple'}),
+     ('Firwin', {'name': 'Windowed FIR', 'mod': 'pyfda.filter_widgets.firwin'}),
+     ('MA', {'name': 'Moving Average', 'mod': 'pyfda.filter_widgets.ma'}),
+     ('Manual_FIR', {'name': 'Manual', 'mod': 'pyfda.filter_widgets.manual'}),
+     ('Manual_IIR', {'name': 'Manual', 'mod': 'pyfda.filter_widgets.manual'})
+     ])
